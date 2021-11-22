d1064103a196b7d71db1aa4c4f3cc8b4aa955382
Angelo Veltens <angelo.veltens@online.de>
2021-11-22 20:56:44 +0100

    thing relations

---
+++ core/src/thing/Thing.relations.spec.ts
@@ -0,0 +1,162 @@
+import { blankNode, graph, sym } from "rdflib";
+import { Thing } from "./Thing";
+
+describe("Thing", function () {
+  describe("relations", () => {
+    it("are empty, if store is empty", () => {
+      const store = graph();
+      const it = new Thing(
+        "https://jane.doe.example/container/file.ttl#fragment",
+        store
+      );
+      expect(it.relations()).toEqual([]);
+    });
+
+    it("contains a single relation from store", () => {
+      const store = graph();
+      const uri = "https://jane.doe.example/container/file.ttl#fragment";
+      store.add(
+        sym(uri),
+        sym("http://vocab.test/predicate"),
+        sym("https://pod.example/resource")
+      );
+      const it = new Thing(
+        "https://jane.doe.example/container/file.ttl#fragment",
+        store
+      );
+      const result = it.relations();
+      expect(result).toEqual([
+        {
+          predicate: "http://vocab.test/predicate",
+          uris: ["https://pod.example/resource"],
+        },
+      ]);
+    });
+
+    it("contains multiple relations from store", () => {
+      const store = graph();
+      const uri = "https://jane.doe.example/container/file.ttl#fragment";
+      store.add(
+        sym(uri),
+        sym("http://vocab.test/first"),
+        sym("https://pod.example/first")
+      );
+      store.add(
+        sym(uri),
+        sym("http://vocab.test/second"),
+        sym("https://pod.example/second")
+      );
+      const it = new Thing(
+        "https://jane.doe.example/container/file.ttl#fragment",
+        store
+      );
+      const result = it.relations();
+      expect(result).toEqual([
+        {
+          predicate: "http://vocab.test/first",
+          uris: ["https://pod.example/first"],
+        },
+        {
+          predicate: "http://vocab.test/second",
+          uris: ["https://pod.example/second"],
+        },
+      ]);
+    });
+
+    it("contains multiple uris for a relation", () => {
+      const store = graph();
+      const uri = "https://jane.doe.example/container/file.ttl#fragment";
+      store.add(
+        sym(uri),
+        sym("http://vocab.test/predicate"),
+        sym("https://pod.example/first")
+      );
+      store.add(
+        sym(uri),
+        sym("http://vocab.test/predicate"),
+        sym("https://pod.example/second")
+      );
+      const it = new Thing(
+        "https://jane.doe.example/container/file.ttl#fragment",
+        store
+      );
+      const result = it.relations();
+      expect(result).toEqual([
+        {
+          predicate: "http://vocab.test/predicate",
+          uris: ["https://pod.example/first", "https://pod.example/second"],
+        },
+      ]);
+    });
+
+    it("contains multiple relations and uris", () => {
+      const store = graph();
+      const uri = "https://jane.doe.example/container/file.ttl#fragment";
+      store.add(
+        sym(uri),
+        sym("http://vocab.test/first"),
+        sym("https://pod.example/first/1")
+      );
+      store.add(
+        sym(uri),
+        sym("http://vocab.test/second"),
+        sym("https://pod.example/second/1")
+      );
+      store.add(
+        sym(uri),
+        sym("http://vocab.test/second"),
+        sym("https://pod.example/second/2")
+      );
+      store.add(
+        sym(uri),
+        sym("http://vocab.test/third"),
+        sym("https://pod.example/third/1")
+      );
+      const it = new Thing(
+        "https://jane.doe.example/container/file.ttl#fragment",
+        store
+      );
+      const result = it.relations();
+      expect(result).toEqual([
+        {
+          predicate: "http://vocab.test/first",
+          uris: ["https://pod.example/first/1"],
+        },
+        {
+          predicate: "http://vocab.test/second",
+          uris: [
+            "https://pod.example/second/1",
+            "https://pod.example/second/2",
+          ],
+        },
+        {
+          predicate: "http://vocab.test/third",
+          uris: ["https://pod.example/third/1"],
+        },
+      ]);
+    });
+
+    it("ignores non named nodes", () => {
+      const store = graph();
+      const uri = "https://jane.doe.example/container/file.ttl#fragment";
+      store.add(
+        sym(uri),
+        sym("http://vocab.test/predicate"),
+        sym("https://pod.example/resource")
+      );
+      store.add(sym(uri), sym("http://vocab.test/literal"), "literal value");
+      store.add(sym(uri), sym("http://vocab.test/blank"), blankNode("blank"));
+      const it = new Thing(
+        "https://jane.doe.example/container/file.ttl#fragment",
+        store
+      );
+      const result = it.relations();
+      expect(result).toEqual([
+        {
+          predicate: "http://vocab.test/predicate",
+          uris: ["https://pod.example/resource"],
+        },
+      ]);
+    });
+  });
+});
--- core/src/thing/Thing.ts
+++ core/src/thing/Thing.ts
@@ -1,4 +1,4 @@
-import { IndexedFormula, isLiteral, Statement, sym } from "rdflib";
+import { IndexedFormula, isLiteral, isNamedNode, Statement, sym } from "rdflib";
 
 export class Thing {
   constructor(readonly uri: string, readonly store: IndexedFormula) {}
@@ -56,6 +56,27 @@ export class Thing {
     return Object.keys(values).map((predicate) => ({
       predicate,
       values: values[predicate],
+    }));
+  }
+
+  relations() {
+    const statements = this.store.statementsMatching(sym(this.uri));
+
+    const values = statements
+      .filter((it) => isNamedNode(it.object))
+      .reduce((accumulator: Accumulator, current: Statement) => {
+        const existing = accumulator[current.predicate.uri];
+        return {
+          ...accumulator,
+          [current.predicate.uri]: existing
+            ? [...existing, current.object.value]
+            : [current.object.value],
+        };
+      }, {});
+
+    return Object.keys(values).map((predicate) => ({
+      predicate,
+      uris: values[predicate],
     }));
   }
 }
