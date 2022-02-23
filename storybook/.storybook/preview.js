1d83d34266a9a2fb2d992c3863eec242763c2774
Angelo Veltens <angelo.veltens@online.de>
2022-02-23 22:57:28 +0100

    setup storybook

---
+++ storybook/.storybook/preview.js
@@ -0,0 +1,22 @@
+import {html} from "lit-html";
+
+export const parameters = {
+  actions: {argTypesRegex: "^on[A-Z].*"},
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+export const decorators = [(story) => html`
+    <pos-app>
+        <ion-content>
+            <div style="padding: 1rem">
+                ${story()}
+            </div>
+        </ion-content>
+    </pos-app>
+`
+]
