d127c838a0e9bfbcaa7e9517a0d840cb31d50222
Angelo Veltens <angelo.veltens@online.de>
2022-07-13 19:15:23 +0200

    accumulate subjects

---
+++ core/src/thing/accumulateSubjects.spec.ts
@@ -0,0 +1,62 @@
+import { Statement, sym } from "rdflib";
+import { accumulateSubjects } from "./accumulateSubjects";
+
+describe("accumulate subjects", () => {
+  it("single value for single predicate", () => {
+    const s: Statement = new Statement(
+      sym("https://subject.test"),
+      sym("https://predicate.test"),
+      sym("https://object.test"),
+      sym("https://graph.test")
+    );
+    const result = [s].reduce(accumulateSubjects, {});
+    expect(result).toEqual({
+      "https://predicate.test": ["https://subject.test"],
+    });
+  });
+
+  it("several predicates", () => {
+    const firstStatement: Statement = new Statement(
+      sym("https://subject1.test"),
+      sym("https://predicate1.test"),
+      sym("https://object.test"),
+      sym("https://graph.test")
+    );
+    const secondStatement: Statement = new Statement(
+      sym("https://subject2.test"),
+      sym("https://predicate2.test"),
+      sym("https://object.test"),
+      sym("https://graph.test")
+    );
+    const result = [firstStatement, secondStatement].reduce(
+      accumulateSubjects,
+      {}
+    );
+    expect(result).toEqual({
+      "https://predicate1.test": ["https://subject1.test"],
+      "https://predicate2.test": ["https://subject2.test"],
+    });
+  });
+
+  it("several subjects for a single predicate", () => {
+    const firstStatement: Statement = new Statement(
+      sym("https://subject1.test"),
+      sym("https://predicate.test"),
+      sym("https://object.test"),
+      sym("https://graph.test")
+    );
+    const secondStatement: Statement = new Statement(
+      sym("https://subject2.test"),
+      sym("https://predicate.test"),
+      sym("https://object.test"),
+      sym("https://graph.test")
+    );
+    const result = [firstStatement, secondStatement].reduce(
+      accumulateSubjects,
+      {}
+    );
+    expect(result).toEqual({
+      "https://predicate.test": ["https://subject1.test", "https://subject2.test"],
+    });
+  });
+});
---
+++ core/src/thing/accumulateSubjects.ts
@@ -0,0 +1,23 @@
+import { Statement } from "rdflib";
+
+interface Accumulator {
+  [key: string]: string[];
+}
+
+/**
+ * accumulate all subject values referencing a resource grouped by predicate
+ * @param accumulator - Target javascript object to accumulate the values to
+ * @param current - A statement with data to add to the accumulator
+ */
+export const accumulateSubjects = (
+  accumulator: Accumulator,
+  current: Statement
+) => {
+  const existing = accumulator[current.predicate.uri];
+  return {
+    ...accumulator,
+    [current.predicate.uri]: existing
+      ? [...existing, current.subject.value]
+      : [current.subject.value],
+  };
+};
