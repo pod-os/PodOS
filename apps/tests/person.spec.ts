83e8f89788a16fd49ebd214e6a459594a6b1bf15
Angelo Veltens <angelo.veltens@online.de>
2022-12-14 21:52:31 +0100

    test: navigate to alice profile via navigation bar

--- apps/tests/person.spec.ts
+++ apps/tests/person.spec.ts
@@ -1,15 +1,18 @@
 import { test, expect } from "@playwright/test";
 
 test("show name as heading for a person", async ({ page }) => {
-  // when navigating to a WebID of a person
-  await page.goto(
-    "/?uri=http%3A%2F%2Flocalhost%3A4000%2Falice%2Fprofile%2Fcard%23me"
-  );
+  // when opening PodOS Browser
+  await page.goto("/");
 
   // then the page title is "PodOS Browser"
   await expect(page).toHaveTitle("PodOS Browser");
 
-  // and the heading shows the name of the person
+  // when I navigate to Alice's WebID
+  const navigationBar = page.getByPlaceholder("Enter URI");
+  await navigationBar.fill("http://localhost:4000/alice/profile/card#me");
+  await navigationBar.press("Enter");
+
+  // then the heading shows Alice's name as heading
   const label = await page.getByRole("heading");
   await expect(label).toHaveText("Alice");
 });
