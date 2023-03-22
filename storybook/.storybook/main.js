a0eded6889f6640437d5c051f4b4295f80c895cf
Angelo Veltens <angelo.veltens@online.de>
2023-03-22 20:14:57 +0100

    chore: update storybook to webpack5 and babel9

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,11 +1,11 @@
 module.exports = {
-  "stories": [
+  core: {
+    builder: "webpack5",
+  },
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/web-components"
-}
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/web-components",
+};
