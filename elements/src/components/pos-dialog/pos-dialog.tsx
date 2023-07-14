1a28f0c54d2a419bff8b35106bececd4d28030cc
Joseph Guillaume <josephguillaume@gmail.com>
2023-07-15 07:51:36 +1000

    prettier

--- elements/src/components/pos-dialog/pos-dialog.tsx
+++ elements/src/components/pos-dialog/pos-dialog.tsx
@@ -1,36 +1,38 @@
-import { Component, Host, h, Prop, Method } from '@stencil/core';
+import { Component, Host, h, Method } from '@stencil/core';
 
 @Component({
-    tag: 'pos-dialog',
-    styleUrl: 'pos-dialog.css',
-    shadow: true,
+  tag: 'pos-dialog',
+  styleUrl: 'pos-dialog.css',
+  shadow: true,
 })
 export class PosDialog {
+  private dialog: HTMLDialogElement;
 
-    private dialog: HTMLDialogElement;
+  @Method()
+  async showModal() {
+    this.dialog.showModal();
+  }
 
-    @Method()
-    async showModal() {
-        this.dialog.showModal();
-    }
+  @Method()
+  async close() {
+    this.dialog.close();
+  }
 
-    @Method()
-    async close() {
-        this.dialog.close();
-    }
-
-    render() {
-        return (
-            <Host>
-                <dialog ref={el => (this.dialog = el as HTMLDialogElement)} >
-                    <header>
-                        <span id="title"><slot /></span>
-                        <button tabindex={-1} id="close" title="Close" onClick={() => this.close()}>
-                            <ion-icon name="close-outline"></ion-icon>
-                        </button>
-                    </header>
-                    <slot />
-                </dialog>
-            </Host>)
-    }
+  render() {
+    return (
+      <Host>
+        <dialog ref={el => (this.dialog = el as HTMLDialogElement)}>
+          <header>
+            <span id="title">
+              <slot />
+            </span>
+            <button tabindex={-1} id="close" title="Close" onClick={() => this.close()}>
+              <ion-icon name="close-outline"></ion-icon>
+            </button>
+          </header>
+          <slot />
+        </dialog>
+      </Host>
+    );
+  }
 }
