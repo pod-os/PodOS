df03354b0bdf572f7124624b01c767dbc76179a5
Angelo Veltens <angelo.veltens@online.de>
2023-08-02 22:02:27 +0200

    fix: add missing mock function

--- elements/src/test/mockPodOS.ts
+++ elements/src/test/mockPodOS.ts
@@ -26,6 +26,7 @@ export function mockPodOS() {
     }),
     proposeUriForNewThing: jest.fn(),
     addNewThing: jest.fn().mockResolvedValue(void null),
+    loadPreferences: jest.fn()
   };
   when(os.store.get)
     .calledWith(alice.webId)
