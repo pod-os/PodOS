038838b73eca38b7c7846e4c9ffc48bfa7c986b0
Angelo Veltens <angelo.veltens@online.de>
2023-09-06 20:06:31 +0200

    feat: inject PodOS to pos-navigation-bar

--- elements/src/components/pos-navigation-bar/pos-navigation-bar.tsx
+++ elements/src/components/pos-navigation-bar/pos-navigation-bar.tsx
@@ -1,15 +1,28 @@
+import { PodOS } from '@pod-os/core';
 import { Component, Event, EventEmitter, h, Prop, State } from '@stencil/core';
+import { PodOsAware, PodOsEventEmitter, subscribePodOs } from '../events/PodOsAware';
 
 @Component({
   tag: 'pos-navigation-bar',
   shadow: true,
 })
-export class PosNavigationBar {
+export class PosNavigationBar implements PodOsAware {
+  @State() os: PodOS;
+
+  @Event({ eventName: 'pod-os:init' }) subscribePodOs: PodOsEventEmitter;
   @Prop() uri: string = '';
 
   @State() value: string = this.uri;
 
   @Event({ eventName: 'pod-os:link' }) linkEmitter: EventEmitter;
+
+  componentWillLoad() {
+    subscribePodOs(this);
+  }
+
+  receivePodOs = async (os: PodOS) => {
+    this.os = os;
+  };
 
   private onChange(event) {
     this.value = event.detail.value;
@@ -23,7 +36,13 @@ export class PosNavigationBar {
   render() {
     return (
       <form onSubmit={e => this.onSubmit(e)}>
-        <ion-searchbar enterkeyhint="search" placeholder="Enter URI" value={this.uri} debounce={0} onIonChange={e => this.onChange(e)} />
+        <ion-searchbar
+          enterkeyhint="search"
+          placeholder="Enter URI"
+          value={this.uri}
+          debounce={0}
+          onIonChange={e => this.onChange(e)}
+        />
       </form>
     );
   }
