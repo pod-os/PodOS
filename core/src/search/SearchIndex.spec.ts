e37dcff9d6755a99ec2101b5d46e8e348cdff16f
Angelo Veltens <angelo.veltens@online.de>
2023-09-20 20:08:08 +0200

    feat: optimize search and allow to find URIs

--- core/src/search/SearchIndex.spec.ts
+++ core/src/search/SearchIndex.spec.ts
@@ -26,32 +26,48 @@ describe("search index", () => {
         const index = new SearchIndex([
           {
             getIndexedItems: () => [
-              { uri: "https://pod.example/item#it", label: "item" },
+              { uri: "https://pod.example/ec51a17#it", label: "item" },
             ],
           } as unknown as LabelIndex,
         ]);
         const result = index.search("item");
         expect(result).toHaveLength(1);
-        expect(result[0].ref).toEqual("https://pod.example/item#it");
+        expect(result[0].ref).toEqual("https://pod.example/ec51a17#it");
+      });
+
+      it("finds an item that is an exact match of two words", () => {
+        const index = new SearchIndex([
+          {
+            getIndexedItems: () => [
+              { uri: "https://pod.example/ec51a17#it", label: "perfect match" },
+            ],
+          } as unknown as LabelIndex,
+        ]);
+        const result = index.search("perfect match");
+        expect(result).toHaveLength(1);
+        expect(result[0].ref).toEqual("https://pod.example/ec51a17#it");
       });
 
       it("exact match is the first match, even if others are similar", () => {
         const index = new SearchIndex([
           {
             getIndexedItems: () => [
-              { uri: "https://pod.example/item#a", label: "Item A" },
-              { uri: "https://pod.example/item#b", label: "Item B" },
-              { uri: "https://pod.example/item#c", label: "Item C" },
+              { uri: "https://pod.example/d11872fd#a", label: "item one" },
+              {
+                uri: "https://pod.example/5e2a2e17#b",
+                label: "item two",
+              },
+              { uri: "https://pod.example/dcc36476#c", label: "item three" },
             ],
           } as unknown as LabelIndex,
         ]);
-        const result = index.search("Item A");
+        const result = index.search("item two");
         expect(result).toHaveLength(3);
-        expect(result[0].ref).toEqual("https://pod.example/item#a");
+        expect(result[0].ref).toEqual("https://pod.example/5e2a2e17#b");
         expect(result[0].score).toBeGreaterThan(result[1].score);
         expect(result[0].score).toBeGreaterThan(result[2].score);
-        expect(result[1].ref).toEqual("https://pod.example/item#b");
-        expect(result[2].ref).toEqual("https://pod.example/item#c");
+        expect(result[1].ref).toEqual("https://pod.example/d11872fd#a");
+        expect(result[2].ref).toEqual("https://pod.example/dcc36476#c");
       });
     });
 
@@ -73,6 +89,65 @@ describe("search index", () => {
         expect(result[0].ref).toEqual("https://pod.example/item#it");
       },
     );
+
+    it("finds mathing URI", () => {
+      const index = new SearchIndex([
+        {
+          getIndexedItems: () => [
+            { uri: "https://pod.example/profile#me", label: "alice" },
+          ],
+        } as unknown as LabelIndex,
+      ]);
+      const result = index.search("https://pod.example/profile#me");
+      expect(result).toHaveLength(1);
+      expect(result[0].ref).toEqual("https://pod.example/profile#me");
+    });
+
+    fit("find labels with dashes", () => {
+      const index = new SearchIndex([
+        {
+          getIndexedItems: () => [
+            {
+              uri: "https://pod.example/thing#it",
+              label: "my-thing",
+            },
+          ],
+        } as unknown as LabelIndex,
+      ]);
+      const result = index.search("my-thing");
+      expect(result).toHaveLength(1);
+      expect(result[0].ref).toEqual("https://pod.example/thing#it");
+    });
+
+    fit("find labels with special chars", () => {
+      const index = new SearchIndex([
+        {
+          getIndexedItems: () => [
+            {
+              uri: "https://pod.example/thing#it",
+              label: "this~is^a+valid:term",
+            },
+          ],
+        } as unknown as LabelIndex,
+      ]);
+      const result = index.search("this~is^a+valid:term");
+      expect(result).toHaveLength(1);
+      expect(result[0].ref).toEqual("https://pod.example/thing#it");
+    });
+
+    // https://github.com/olivernn/lunr.js/issues/256#issuecomment-294935042
+    it("finds Alice, even if stemmed to alic", () => {
+      const index = new SearchIndex([
+        {
+          getIndexedItems: () => [
+            { uri: "https://pod.example/profile#me", label: "Alice" },
+          ],
+        } as unknown as LabelIndex,
+      ]);
+      const result = index.search("alice");
+      expect(result).toHaveLength(1);
+      expect(result[0].ref).toEqual("https://pod.example/profile#me");
+    });
   });
 
   describe("clear", () => {
