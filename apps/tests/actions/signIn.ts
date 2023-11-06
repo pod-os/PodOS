62070fe59c58bedecd14e036d6004b1959f4f525
Angelo Veltens <angelo.veltens@online.de>
2023-11-06 21:35:16 +0100

    test: upgrade e2e test server to CSS 7

--- apps/tests/actions/signIn.ts
+++ apps/tests/actions/signIn.ts
@@ -18,11 +18,21 @@ export async function signIn(
   const loginButton = page.locator("pos-login").getByRole("button");
   await loginButton.click();
 
-  await expect(page).toHaveURL("http://localhost:4000/idp/login/");
+  await expect(page).toHaveURL(
+    "http://localhost:4000/.account/login/password/",
+  );
 
   await page.getByLabel("Email").type(credentials.email);
   await page.getByLabel("Password").type(credentials.password);
   await page.getByRole("button", { name: "Log in" }).click();
+
+  await page
+    .getByRole("radio", {
+      name: "http://localhost:4000/alice/profile/card#me",
+    })
+    .click();
+
+  await expect(page.getByText("Pod OS at localhost:3000")).toBeVisible();
   await page.getByRole("button", { name: "Authorize" }).click();
 
   await expect(page).toHaveURL(originalLocation);
