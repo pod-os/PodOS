f22ff2fc70a81ea396d286a6b2f6f2351ef5b427
Angelo Veltens <angelo.veltens@online.de>
2024-01-13 07:57:45 +0100

    chore: remove workarround for https://github.com/CommunitySolidServer/CommunitySolidServer/issues/1768

--- apps/tests/actions/signIn.ts
+++ apps/tests/actions/signIn.ts
@@ -22,17 +22,10 @@ export async function signIn(
     "http://localhost:4000/.account/login/password/",
   );
 
-  await page.getByLabel("Email").type(credentials.email);
-  await page.getByLabel("Password").type(credentials.password);
+  await page.getByLabel("Email").fill(credentials.email);
+  await page.getByLabel("Password").fill(credentials.password);
   await page.getByRole("button", { name: "Log in" }).click();
 
-  await page
-    .getByRole("radio", {
-      name: "http://localhost:4000/alice/profile/card#me",
-    })
-    .click();
-
-  await expect(page.getByText("Pod OS at localhost")).toBeVisible();
   await page.getByRole("button", { name: "Authorize" }).click();
 
   await expect(page).toHaveURL(originalLocation);
