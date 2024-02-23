9d80e4cd9c79fa92058bf4eb1734eb7d2b167f0c
Angelo Veltens <angelo.veltens@online.de>
2024-02-23 18:19:51 +0100

    chore(contacts): upgrade to contacts module 0.4.0

--- core/src/modules/contacts.ts
+++ core/src/modules/contacts.ts
@@ -4,10 +4,8 @@ import { Store } from "../Store";
 export async function loadContactsModule(
   store: Store,
 ): Promise<ContactsModule> {
-  const { ContactsModuleRdfLib } = await import(
-    "@solid-data-modules/contacts-rdflib"
-  );
-  return new ContactsModuleRdfLib({
+  const module = await import("@solid-data-modules/contacts-rdflib");
+  return new module.default({
     store: store.graph,
     fetcher: store.fetcher,
     updater: store.updater,
