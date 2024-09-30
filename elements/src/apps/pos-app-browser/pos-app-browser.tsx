af55aba8108b0ca80595c26b965ad234f48c8046
Angelo Veltens <angelo+github.com@veltens.org>
2024-09-30 21:51:20 +0200

    feat: add logo to footer

--- elements/src/apps/pos-app-browser/pos-app-browser.tsx
+++ elements/src/apps/pos-app-browser/pos-app-browser.tsx
@@ -25,7 +25,10 @@ export class PosAppBrowser {
                   <pos-type-router />
                 </pos-resource>
               </main>
-              <footer>PodOS Browser</footer>
+              <footer>
+                <Logo />
+                <span>PodOS Browser</span>
+              </footer>
             </pos-router>
           </pos-error-toast>
         </pos-app>
@@ -33,3 +36,39 @@ export class PosAppBrowser {
     );
   }
 }
+
+const Logo = () => {
+  return (
+    <svg viewBox="0 0 400 400" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M50 350 L50 100 L200 50 L350 100 L350 350 Z"
+        style={{
+          'fill': '#008BF8',
+          'stroke': '#008BF8',
+          'stroke-width': '70',
+        }}
+        stroke-linejoin="round"
+      />
+
+      <path
+        d="M100 300 L100 150 L200 120 L300 150 L300 300 L130 300 L130 370 L100 370Z"
+        style={{
+          'fill': 'white',
+          'stroke': 'white',
+          'stroke-width': '45',
+        }}
+        stroke-linejoin="round"
+      />
+
+      <path
+        d="M150 250 L150 200 L200 180 L250 200 L250 250 Z"
+        style={{
+          'fill': '#DC0073',
+          'stroke': '#DC0073',
+          'stroke-width': '20',
+        }}
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
