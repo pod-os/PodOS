855292b2ea59e32097921a212b7161da578a782e
Angelo Veltens <angelo+github.com@veltens.org>
2024-10-21 20:30:06 +0200

    feat: hide dashboard URI in navigation bar, so that users can see the placeholder text

--- elements/src/apps/pos-app-browser/pos-app-browser.spec.tsx
+++ elements/src/apps/pos-app-browser/pos-app-browser.spec.tsx
@@ -3,7 +3,7 @@ import { PosAppBrowser } from './pos-app-browser';
 import { getByRole } from '@testing-library/dom';
 
 describe('pos-app-browser', () => {
-  it('uses internal router for internal URIs ', async () => {
+  it('uses internal router for internal URIs', async () => {
     const page = await newSpecPage({
       components: [PosAppBrowser],
       html: `<pos-app-browser />`,
@@ -20,6 +20,54 @@ describe('pos-app-browser', () => {
         <pos-internal-router uri="pod-os:dashboard"></pos-internal-router>
       </main>
     `);
+  });
+
+  it('hides uri in navigation bar, if visiting dashboard', async () => {
+    const page = await newSpecPage({
+      components: [PosAppBrowser],
+      html: `<pos-app-browser />`,
+    });
+
+    page.rootInstance.uri = 'pod-os:dashboard';
+
+    await page.waitForChanges();
+
+    const main = getByRole(page.root, 'banner');
+    const navigationBar = main.querySelector('pos-navigation-bar');
+
+    expect(navigationBar).toEqualAttribute('uri', '');
+  });
+
+  it('shows uri in navigation bar, if visiting other internal pages', async () => {
+    const page = await newSpecPage({
+      components: [PosAppBrowser],
+      html: `<pos-app-browser />`,
+    });
+
+    page.rootInstance.uri = 'pod-os:other';
+
+    await page.waitForChanges();
+
+    const main = getByRole(page.root, 'banner');
+    const navigationBar = main.querySelector('pos-navigation-bar');
+
+    expect(navigationBar).toEqualAttribute('uri', 'pod-os:other');
+  });
+
+  it('shows uri in navigation bar, if visiting http(s) URIs', async () => {
+    const page = await newSpecPage({
+      components: [PosAppBrowser],
+      html: `<pos-app-browser />`,
+    });
+
+    page.rootInstance.uri = 'https://resource.test';
+
+    await page.waitForChanges();
+
+    const main = getByRole(page.root, 'banner');
+    const navigationBar = main.querySelector('pos-navigation-bar');
+
+    expect(navigationBar).toEqualAttribute('uri', 'https://resource.test');
   });
 
   it('uses type router for http(s) URIs ', async () => {
