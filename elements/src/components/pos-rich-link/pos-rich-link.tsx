b5703d55239ced4ddc3e65384484af6c43f4f90b
Angelo Veltens <angelo+github.com@veltens.org>
2024-10-23 19:27:19 +0200

    feat: only link the label of pos-rich-link

--- elements/src/components/pos-rich-link/pos-rich-link.tsx
+++ elements/src/components/pos-rich-link/pos-rich-link.tsx
@@ -12,21 +12,20 @@ export class PosRichLink {
 
   render() {
     return (
-      <a
-        class="container"
-        href={this.uri}
-        onClick={e => {
-          e.preventDefault();
-          this.linkEmitter.emit(this.uri);
-        }}
-      >
-        <pos-resource lazy={true} uri={this.uri}>
-          <p class="content">
+      <pos-resource lazy={true} uri={this.uri}>
+        <p class="content">
+          <a
+            href={this.uri}
+            onClick={e => {
+              e.preventDefault();
+              this.linkEmitter.emit(this.uri);
+            }}
+          >
             <pos-label />
-            <pos-description />
-          </p>
-        </pos-resource>
-      </a>
+          </a>
+          <pos-description />
+        </p>
+      </pos-resource>
     );
   }
 }
