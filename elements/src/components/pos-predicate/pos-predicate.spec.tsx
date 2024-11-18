bc30eec4486021342ccde9fc0dca77ac2ec673bc
Angelo Veltens <angelo+github.com@veltens.org>
2024-11-18 21:17:53 +0100

    feat(elements): allow to expand pos-predicate to show and link to full URI

--- elements/src/components/pos-predicate/pos-predicate.spec.tsx
+++ elements/src/components/pos-predicate/pos-predicate.spec.tsx
@@ -1,18 +1,63 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { PosPredicate } from './pos-predicate';
+import { getByRole } from '@testing-library/dom';
 
 describe('pos-predicate', () => {
   it('renders', async () => {
     const page = await newSpecPage({
       components: [PosPredicate],
+      supportsShadowDom: false,
       html: `<pos-predicate uri="https://predicate.test" label="test-label" />`,
     });
 
     expect(page.root).toEqualHtml(`
      <pos-predicate label="test-label" uri="https://predicate.test">
-      <ion-label title="https://predicate.test">
+      <button title="https://predicate.test">
         test-label
-      </ion-label>
+      </button>
+    </pos-predicate>
+    `);
+  });
+
+  it('expands to full URI when clicked', async () => {
+    const page = await newSpecPage({
+      components: [PosPredicate],
+      html: `<pos-predicate uri="https://predicate.test" label="test-label" />`,
+      supportsShadowDom: false,
+    });
+    const button = getByRole(page.root, 'button');
+    button.click();
+    await page.waitForChanges();
+    expect(page.root).toEqualHtml(`
+     <pos-predicate label="test-label" uri="https://predicate.test">
+      <div>
+        <a href="https://predicate.test">
+          https://predicate.test
+        </a>
+        <button>
+          <ion-icon name="chevron-back-circle-outline"></ion-icon>
+        </button>
+      </div>
+    </pos-predicate>
+    `);
+  });
+
+  it('collapses to short label if collapse button clicked', async () => {
+    const page = await newSpecPage({
+      components: [PosPredicate],
+      supportsShadowDom: false,
+      html: `<pos-predicate uri="https://predicate.test" label="test-label" />`,
+    });
+    page.rootInstance.expanded = true;
+    await page.waitForChanges();
+    const button = getByRole(page.root, 'button');
+    button.click();
+    await page.waitForChanges();
+    expect(page.root).toEqualHtml(`
+     <pos-predicate label="test-label" uri="https://predicate.test">
+      <button title="https://predicate.test">
+        test-label
+      </button>
     </pos-predicate>
     `);
   });
--- elements/src/components/pos-predicate/pos-predicate.tsx
+++ elements/src/components/pos-predicate/pos-predicate.tsx
@@ -1,7 +1,8 @@
-import { Component, h, Prop } from '@stencil/core';
+import { Component, h, Prop, State } from '@stencil/core';
 
 @Component({
   tag: 'pos-predicate',
+  shadow: true,
 })
 export class PosPredicate {
   @Prop()
@@ -10,7 +11,25 @@ export class PosPredicate {
   @Prop()
   label: string;
 
+  @State()
+  expanded: boolean = false;
+
   render() {
-    return <ion-label title={this.uri}>{this.label}</ion-label>;
+    if (this.expanded) {
+      return (
+        <div>
+          <a href={this.uri}>{this.uri}</a>
+          <button onClick={() => (this.expanded = false)}>
+            <ion-icon name="chevron-back-circle-outline"></ion-icon>
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <button onClick={() => (this.expanded = true)} title={this.uri}>
+          {this.label}
+        </button>
+      );
+    }
   }
 }
