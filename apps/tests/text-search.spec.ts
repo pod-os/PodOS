01c71c6cbdd5e1ab7e2779c172d70949e787a449
Angelo Veltens <angelo+github.com@veltens.org>
2025-03-31 22:01:55 +0200

    test(e2e): can find a thing after making it findable

--- apps/tests/text-search.spec.ts
+++ apps/tests/text-search.spec.ts
@@ -28,4 +28,40 @@ test.describe("Text search", () => {
       );
     });
   });
+
+  test("can find a thing, after making it findable", async ({ page }) => {
+    await test.step("given a user is signed in", async () => {
+      await page.goto("/");
+      await signIn(page, alice);
+    });
+
+    await test.step("and there is no search result for 'Banana'", async () => {
+      const navigationBar = page.getByPlaceholder("Search or enter URI");
+      await navigationBar.fill("Banana");
+
+      const result = page.getByRole("listitem").filter({ hasText: "Banana" });
+      await expect(result).not.toBeVisible();
+    });
+
+    await test.step("when they navigate to the thing", async () => {
+      const navigationBar = page.getByPlaceholder("Enter URI");
+      await navigationBar.fill(
+        "http://localhost:4000/alice/make-findable/banana#it",
+      );
+      await navigationBar.press("Enter");
+      await expect(page.getByRole("heading", { name: "Banana" })).toBeVisible();
+    });
+
+    await test.step("and they make it findable", async () => {
+      await page.getByRole("button", { name: "Make this findable" }).click();
+    });
+
+    await test.step("then they will find it when retrying the search", async () => {
+      const navigationBar = page.getByPlaceholder("Search or enter URI");
+      await navigationBar.fill("Banana");
+
+      const result = page.getByRole("listitem").filter({ hasText: "Banana" });
+      await expect(result).toBeVisible();
+    });
+  });
 });
