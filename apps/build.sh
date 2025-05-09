c576356a37cd4a6438b4f97c3905b349f83e45f0
Angelo Veltens <angelo+github.com@veltens.org>
2025-05-09 16:37:10 +0200

    style: generate favicon and increase padding for app icons, so they are not cut off

--- apps/build.sh
+++ apps/build.sh
@@ -21,6 +21,24 @@ echo "Generating manifest.json..."
 envsubst < "${POD_OS_APP_NAME}".manifest.json > dist/"${POD_OS_APP_NAME}"/manifest.json
 
 echo "Generating PWA assets"
-npx pwa-asset-generator ../assets/logo.png ./dist/"${POD_OS_APP_NAME}"/icons --index ./dist/"${POD_OS_APP_NAME}"/index.html --manifest ./dist/"${POD_OS_APP_NAME}"/manifest.json
+npx pwa-asset-generator \
+  --opaque false \
+  --favicon \
+  --padding "0" \
+  --icon-only \
+  --index ./dist/"${POD_OS_APP_NAME}"/index.html \
+  ../assets/logo.svg \
+  ./dist/"${POD_OS_APP_NAME}"/icons
+
+npx pwa-asset-generator \
+  --opaque false \
+  --padding "calc(50vh - 25%) calc(50vw - 50%)" \
+  --index ./dist/"${POD_OS_APP_NAME}"/index.html \
+  --manifest ./dist/"${POD_OS_APP_NAME}"/manifest.json \
+  ../assets/logo.svg \
+  ./dist/"${POD_OS_APP_NAME}"/icons
+
+
+
 
 echo "All done! 🎉"
