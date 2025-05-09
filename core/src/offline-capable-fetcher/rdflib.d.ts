408e7fb80e7825869207488108cfdb617a891f32
Angelo Veltens <angelo+github.com@veltens.org>
2025-05-09 21:21:06 +0200

    feat: extend rdflib Fetcher to be able to add offline functionality

---
+++ core/src/offline-capable-fetcher/rdflib.d.ts
@@ -0,0 +1,20 @@
+// Import the original types from rdflib
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as rdflib from "rdflib";
+
+/**
+ * This module declaration is needed as a workarround, because
+ * rdflib does not export all the types used in the method signatures of the Fetcher.
+ * We are changing the signatures here to something that is available and (hopefully) matches with
+ * what the Fetcher is actually compatible with
+ */
+declare module "rdflib" {
+  export class Fetcher {
+    constructor(store: IndexedFormula, options?: object);
+
+    load<T extends NamedNode | string | Array<string | NamedNode>>(
+      url: T,
+      options?: object,
+    ): Promise<Response>;
+  }
+}
