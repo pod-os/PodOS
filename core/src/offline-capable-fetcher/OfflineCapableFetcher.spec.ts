d48c7b9d1a08fac05289447005c2c06dab1a32f5
Angelo Veltens <angelo+github.com@veltens.org>
2025-05-11 21:29:32 +0200

    feat: handle different types of uri parameter as original rdflib fetcher

--- core/src/offline-capable-fetcher/OfflineCapableFetcher.spec.ts
+++ core/src/offline-capable-fetcher/OfflineCapableFetcher.spec.ts
@@ -4,81 +4,230 @@ import { when } from "jest-when";
 import { OfflineCache } from "./OfflineCache";
 
 describe(OfflineCapableFetcher.name, () => {
-  it("loads data to the store from network normally", async () => {
-    // given a turtle document can be fetched
-    const fetch = jest.fn();
-    mockTurtleDocument(
-      fetch,
-      "https://alice.pod.test/thing",
-      `
+  describe("while online", () => {
+    it.each([
+      {
+        argumentType: "uri string",
+        uriArg: "https://alice.pod.test/thing#it",
+      },
+      {
+        argumentType: "NamedNode",
+        uriArg: sym("https://alice.pod.test/thing#it"),
+      },
+    ])(
+      `fetches document for given $argumentType and puts data to store and offline cache`,
+      async ({ uriArg }) => {
+        // given a turtle document can be fetched
+        const fetch = jest.fn();
+        mockTurtleDocument(
+          fetch,
+          "https://alice.pod.test/thing",
+          `
       <https://alice.pod.test/thing#it> <http://www.w3.org/2000/01/rdf-schema#label> "A thing".
     `,
-      { etag: 'W/"1234567890"' },
+          { etag: 'W/"1234567890"' },
+        );
+        const store = graph();
+
+        // and a fetcher that is currently online
+        const offlineCache = {
+          put: jest.fn(),
+        } as OfflineCache;
+        const fetcher = new OfflineCapableFetcher(store, {
+          fetch,
+          offlineCache,
+          isOnline: () => true,
+        });
+
+        // when the fetcher loads a resource from that document
+        const response = await fetcher.load(uriArg as string); // typecast to string is a workaround to satisfy the type checker
+
+        // then the etag can be read from the response
+        expect(response.headers.get("etag")).toEqual('W/"1234567890"');
+
+        // and all the statements from the document are in the store
+        const statementsInStore = store.statementsMatching(
+          null,
+          null,
+          null,
+          sym("https://alice.pod.test/thing"),
+        );
+        expect(statementsInStore).toEqual([
+          {
+            graph: {
+              classOrder: 5,
+              termType: "NamedNode",
+              value: "https://alice.pod.test/thing",
+            },
+            object: {
+              classOrder: 1,
+              datatype: {
+                classOrder: 5,
+                termType: "NamedNode",
+                value: "http://www.w3.org/2001/XMLSchema#string",
+              },
+              isVar: 0,
+              language: "",
+              termType: "Literal",
+              value: "A thing",
+            },
+            predicate: {
+              classOrder: 5,
+              termType: "NamedNode",
+              value: "http://www.w3.org/2000/01/rdf-schema#label",
+            },
+            subject: {
+              classOrder: 5,
+              termType: "NamedNode",
+              value: "https://alice.pod.test/thing#it",
+            },
+          },
+        ]);
+
+        // and the statements have been added to the offline cache using etag as revision
+        expect(offlineCache.put).toHaveBeenCalledWith({
+          url: "https://alice.pod.test/thing",
+          revision: 'W/"1234567890"',
+          statements: [
+            '<https://alice.pod.test/thing#it> <http://www.w3.org/2000/01/rdf-schema#label> "A thing" .',
+          ],
+        });
+      },
     );
-    const store = graph();
 
-    // and a fetcher that is currently online
-    const offlineCache = {
-      put: jest.fn(),
-    } as OfflineCache;
-    const fetcher = new OfflineCapableFetcher(store, {
-      fetch,
-      offlineCache,
-      isOnline: () => true,
-    });
+    it("fetches multiple documents if array is given and puts all the data to the store and offline cache", async () => {
+      // given two turtle documents can be fetched
+      const fetch = jest.fn();
+      mockTurtleDocument(
+        fetch,
+        "https://alice.pod.test/one",
+        `
+      <https://alice.pod.test/one#it> <http://www.w3.org/2000/01/rdf-schema#label> "First".
+    `,
+        { etag: "etag-doc-1" },
+      );
+      mockTurtleDocument(
+        fetch,
+        "https://alice.pod.test/two",
+        `
+      <https://alice.pod.test/two#it> <http://www.w3.org/2000/01/rdf-schema#label> "Second".
+    `,
+        { etag: "etag-doc-2" },
+      );
+      const store = graph();
 
-    // when the fetcher loads a resource from that document
-    const response = await fetcher.load("https://alice.pod.test/thing#it");
+      // and a fetcher that is currently online
+      const offlineCache = {
+        put: jest.fn(),
+      } as OfflineCache;
+      const fetcher = new OfflineCapableFetcher(store, {
+        fetch,
+        offlineCache,
+        isOnline: () => true,
+      });
 
-    // then the etag can be read from the response
-    expect(response.headers.get("etag")).toEqual('W/"1234567890"');
+      // when the fetcher loads resources from those documents in parallel
+      const responses = await fetcher.load([
+        "https://alice.pod.test/one#it", // one uri given as string
+        sym("https://alice.pod.test/two#it"), // another uri given as NamedNode
+      ]);
 
-    // and all the statements from the document are in the store
-    const statementsInStore = store.statementsMatching(
-      null,
-      null,
-      null,
-      sym("https://alice.pod.test/thing"),
-    );
-    expect(statementsInStore).toEqual([
-      {
-        graph: {
-          classOrder: 5,
-          termType: "NamedNode",
-          value: "https://alice.pod.test/thing",
-        },
-        object: {
-          classOrder: 1,
-          datatype: {
+      // then the etag can be read from the responses
+      expect(responses[0].headers.get("etag")).toEqual("etag-doc-1");
+      expect(responses[1].headers.get("etag")).toEqual("etag-doc-2");
+
+      // and all the statements from the documents are in the store
+      const statementsOfFirstDoc = store.statementsMatching(
+        null,
+        null,
+        null,
+        sym("https://alice.pod.test/one"),
+      );
+      const statementsOfSecondDoc = store.statementsMatching(
+        null,
+        null,
+        null,
+        sym("https://alice.pod.test/two"),
+      );
+      expect(statementsOfFirstDoc).toEqual([
+        {
+          graph: {
             classOrder: 5,
             termType: "NamedNode",
-            value: "http://www.w3.org/2001/XMLSchema#string",
+            value: "https://alice.pod.test/one",
           },
-          isVar: 0,
-          language: "",
-          termType: "Literal",
-          value: "A thing",
+          object: {
+            classOrder: 1,
+            datatype: {
+              classOrder: 5,
+              termType: "NamedNode",
+              value: "http://www.w3.org/2001/XMLSchema#string",
+            },
+            isVar: 0,
+            language: "",
+            termType: "Literal",
+            value: "First",
+          },
+          predicate: {
+            classOrder: 5,
+            termType: "NamedNode",
+            value: "http://www.w3.org/2000/01/rdf-schema#label",
+          },
+          subject: {
+            classOrder: 5,
+            termType: "NamedNode",
+            value: "https://alice.pod.test/one#it",
+          },
         },
-        predicate: {
-          classOrder: 5,
-          termType: "NamedNode",
-          value: "http://www.w3.org/2000/01/rdf-schema#label",
-        },
-        subject: {
-          classOrder: 5,
-          termType: "NamedNode",
-          value: "https://alice.pod.test/thing#it",
-        },
-      },
-    ]);
+      ]);
 
-    // and the statements have been added to the offline cache using etag as revision
-    expect(offlineCache.put).toHaveBeenCalledWith({
-      url: "https://alice.pod.test/thing",
-      revision: 'W/"1234567890"',
-      statements: [
-        '<https://alice.pod.test/thing#it> <http://www.w3.org/2000/01/rdf-schema#label> "A thing" .',
-      ],
+      expect(statementsOfSecondDoc).toEqual([
+        {
+          graph: {
+            classOrder: 5,
+            termType: "NamedNode",
+            value: "https://alice.pod.test/two",
+          },
+          object: {
+            classOrder: 1,
+            datatype: {
+              classOrder: 5,
+              termType: "NamedNode",
+              value: "http://www.w3.org/2001/XMLSchema#string",
+            },
+            isVar: 0,
+            language: "",
+            termType: "Literal",
+            value: "Second",
+          },
+          predicate: {
+            classOrder: 5,
+            termType: "NamedNode",
+            value: "http://www.w3.org/2000/01/rdf-schema#label",
+          },
+          subject: {
+            classOrder: 5,
+            termType: "NamedNode",
+            value: "https://alice.pod.test/two#it",
+          },
+        },
+      ]);
+
+      // and the statements have been added to the offline cache using etag as revision
+      expect(offlineCache.put).toHaveBeenCalledWith({
+        url: "https://alice.pod.test/one",
+        revision: "etag-doc-1",
+        statements: [
+          '<https://alice.pod.test/one#it> <http://www.w3.org/2000/01/rdf-schema#label> "First" .',
+        ],
+      });
+      expect(offlineCache.put).toHaveBeenCalledWith({
+        url: "https://alice.pod.test/two",
+        revision: "etag-doc-2",
+        statements: [
+          '<https://alice.pod.test/two#it> <http://www.w3.org/2000/01/rdf-schema#label> "Second" .',
+        ],
+      });
     });
   });
 });
--- core/src/offline-capable-fetcher/OfflineCapableFetcher.ts
+++ core/src/offline-capable-fetcher/OfflineCapableFetcher.ts
@@ -27,14 +27,34 @@ export class OfflineCapableFetcher extends Fetcher {
     this.offlineCache = offlineCache;
   }
 
-  async load<T extends NamedNode | string | Array<string | NamedNode>>(
-    uri: T,
+  async load(
+    uri: string,
     options?: Partial<AutoInitOptions>,
-  ) {
-    const doc = sym(termValue(uri as NamedNode)).doc();
-    const response = await super.load(uri, options);
-    const etag = response.headers.get("etag");
+  ): Promise<Response>;
+  async load(
+    uri: NamedNode,
+    options?: Partial<AutoInitOptions>,
+  ): Promise<Response>;
+  async load(
+    uri: Array<string | NamedNode>,
+    options?: Partial<AutoInitOptions>,
+  ): Promise<Response[]>;
+  async load<T extends NamedNode | string | Array<string | NamedNode>>(
+    oneOrMoreUris: T,
+    options?: object,
+  ): Promise<T extends Array<string | NamedNode> ? Response[] : Response> {
+    if (Array.isArray(oneOrMoreUris)) {
+      // TODO test this case
+      return (await super.load<Array<string | NamedNode>>(
+        oneOrMoreUris,
+        options,
+      )) as T extends Array<string | NamedNode> ? Response[] : Response;
+    }
+    const node = sym(termValue(oneOrMoreUris)); // make sure we get a NamedNode
+    const doc = node.doc();
+    const response = await super.load(node, options);
 
+    const etag = response.headers.get("etag");
     const statementsInStore = this.store.statementsMatching(
       null,
       null,
@@ -47,6 +67,8 @@ export class OfflineCapableFetcher extends Fetcher {
       revision: etag || "",
       statements: statementsInStore.map((s) => s.toNT()),
     });
-    return response;
+    return response as T extends Array<string | NamedNode>
+      ? Response[]
+      : Response;
   }
 }
