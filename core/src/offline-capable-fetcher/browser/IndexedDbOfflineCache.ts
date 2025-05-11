b5937b64715e0437807932515475e0619403d8f0
Angelo Veltens <angelo+github.com@veltens.org>
2025-05-11 22:45:19 +0200

    refactor: add type safety to db

--- core/src/offline-capable-fetcher/browser/IndexedDbOfflineCache.ts
+++ core/src/offline-capable-fetcher/browser/IndexedDbOfflineCache.ts
@@ -1,11 +1,18 @@
 import { CachedRdfDocument, OfflineCache } from "../OfflineCache";
-import { IDBPDatabase, openDB } from "idb";
+import { DBSchema, IDBPDatabase, openDB } from "idb";
+
+interface OfflineCacheDb extends DBSchema {
+  documents: {
+    key: string;
+    value: CachedRdfDocument;
+  };
+}
 
 export class IndexedDbOfflineCache implements OfflineCache {
-  private readonly dbPromise: Promise<IDBPDatabase>;
+  private readonly dbPromise: Promise<IDBPDatabase<OfflineCacheDb>>;
 
   constructor() {
-    this.dbPromise = openDB("OfflineCacheDB", 1, {
+    this.dbPromise = openDB<OfflineCacheDb>("OfflineCacheDB", 1, {
       upgrade(db) {
         if (!db.objectStoreNames.contains("documents")) {
           db.createObjectStore("documents", { keyPath: "url" });
@@ -16,9 +23,7 @@ export class IndexedDbOfflineCache implements OfflineCache {
 
   async put(document: CachedRdfDocument): Promise<void> {
     const db = await this.dbPromise;
-    const existing = (await db.get("documents", document.url)) as
-      | CachedRdfDocument
-      | undefined;
+    const existing = await db.get("documents", document.url);
     if (existing && existing.revision === document.revision) {
       return; // No need to update if the revision is the same
     }
