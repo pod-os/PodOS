05911c1a3523ba5963613c211f272b32410ee6f9
Angelo Veltens <angelo+github.com@veltens.org>
2025-05-11 22:40:59 +0200

    feat: only update cache if revision changes

--- core/src/offline-capable-fetcher/browser/IndexDbOfflineCache.spec.ts
+++ core/src/offline-capable-fetcher/browser/IndexDbOfflineCache.spec.ts
@@ -7,18 +7,26 @@ jest.mock("idb");
 
 describe("IndexDbOfflineCache", () => {
   describe("put", () => {
-    it("should store document in IndexDB", async () => {
-      // given openDB opens a new indexeddb database
-      const db = {
+    let db: IDBPDatabase;
+    let cache: IndexedDbOfflineCache;
+
+    beforeEach(() => {
+      db = {
         put: jest.fn(),
+        get: jest.fn(),
       } as unknown as IDBPDatabase;
       const dbPromise = Promise.resolve(db);
       when(openDB)
         .calledWith("OfflineCacheDB", 1, expect.anything())
         .mockReturnValue(dbPromise);
+      cache = new IndexedDbOfflineCache();
+    });
 
-      // and an offline cached using IndexedDB
-      const cache = new IndexedDbOfflineCache();
+    it("should store document in IndexDB if not present yet", async () => {
+      // given no existing document in cache
+      when(db.get)
+        .calledWith("documents", "https://document.example/")
+        .mockResolvedValue(undefined);
 
       // and a document to store
       const document: CachedRdfDocument = {
@@ -32,6 +40,55 @@ describe("IndexDbOfflineCache", () => {
 
       // then it is put to the IndexedDB documents store
       expect(db.put).toHaveBeenCalledWith("documents", document);
+    });
+
+    it("should store document in IndexDB when revision is new", async () => {
+      // given no existing document in cache
+      when(db.get)
+        .calledWith("documents", "https://document.example/")
+        .mockResolvedValue({
+          url: "https://document.example/",
+          statements: "test content",
+          revision: "old-revision",
+        });
+
+      // and a document to store
+      const document: CachedRdfDocument = {
+        url: "https://document.example/",
+        statements: "test content",
+        revision: "new-revision",
+      };
+
+      // when the document is put to the cache
+      await cache.put(document);
+
+      // then it is put to the IndexedDB documents store
+      expect(db.put).toHaveBeenCalledWith("documents", document);
+    });
+
+    it("should not store document when revision is still the same", async () => {
+      // given an existing document in cache
+      const existingDoc: CachedRdfDocument = {
+        url: "https://document.example/",
+        statements: "known content",
+        revision: "known-revision",
+      };
+      when(db.get)
+        .calledWith("documents", "https://document.example/")
+        .mockResolvedValue(existingDoc);
+
+      // and a document with same revision
+      const document: CachedRdfDocument = {
+        url: "https://document.example/",
+        statements: "might actually change, but we trust the revision",
+        revision: "known-revision",
+      };
+
+      // when the document is put to the cache
+      await cache.put(document);
+
+      // then it is not put to the IndexedDB
+      expect(db.put).not.toHaveBeenCalled();
     });
   });
 });
--- core/src/offline-capable-fetcher/browser/IndexedDbOfflineCache.ts
+++ core/src/offline-capable-fetcher/browser/IndexedDbOfflineCache.ts
@@ -16,7 +16,12 @@ export class IndexedDbOfflineCache implements OfflineCache {
 
   async put(document: CachedRdfDocument): Promise<void> {
     const db = await this.dbPromise;
+    const existing = (await db.get("documents", document.url)) as
+      | CachedRdfDocument
+      | undefined;
+    if (existing && existing.revision === document.revision) {
+      return; // No need to update if the revision is the same
+    }
     await db.put("documents", document);
-    console.log("IndexDbOfflineCache: put", document);
   }
 }
