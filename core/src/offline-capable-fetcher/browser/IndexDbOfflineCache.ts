c6b44afd2caa05ed4570e9c0e628a659d4d23da1
Angelo Veltens <angelo+github.com@veltens.org>
2025-05-11 22:24:13 +0200

    feat: store cached documents in IndexedDB

--- core/src/offline-capable-fetcher/browser/IndexDbOfflineCache.ts
+++
@@ -1,7 +0,0 @@
-import { CachedRdfDocument, OfflineCache } from "../OfflineCache";
-
-export class IndexDbOfflineCache implements OfflineCache {
-  put(document: CachedRdfDocument): void {
-    console.log("IndexDbOfflineCache: put", document);
-  }
-}
