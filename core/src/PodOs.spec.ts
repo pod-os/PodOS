61457cf919c9e34e310ed1ce7cab9a16116921ca
Angelo Veltens <angelo+github.com@veltens.org>
2025-05-19 20:46:09 +0200

    feat: trigger clearing offline cache on logout

---
+++ core/src/PodOs.spec.ts
@@ -0,0 +1,35 @@
+import { BrowserSession, OfflineCache, PodOS } from "./index";
+import { of } from "rxjs";
+
+describe("PodOS", () => {
+  let mockSession: BrowserSession;
+
+  beforeEach(() => {
+    mockSession = {
+      logout: jest.fn(),
+      observeSession: jest.fn().mockReturnValue(of()),
+    } as unknown as BrowserSession;
+  });
+
+  describe("logout", () => {
+    it("calls logout on the browser session", async () => {
+      const podOs = new PodOS({ session: mockSession });
+
+      await podOs.logout();
+
+      expect(mockSession.logout).toHaveBeenCalled();
+    });
+
+    it("clears the cache", async () => {
+      const mockOfflineCache = { clear: jest.fn() } as unknown as OfflineCache;
+      const podOs = new PodOS({
+        session: mockSession,
+        offlineCache: mockOfflineCache,
+      });
+
+      await podOs.logout();
+
+      expect(mockOfflineCache.clear).toHaveBeenCalled();
+    });
+  });
+});
