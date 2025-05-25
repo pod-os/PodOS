602fa420f9d0940c432280adf659797e58bcd032
jg10 <181532694+jg10-mastodon-social@users.noreply.github.com>
2025-05-25 14:48:28 +1000

    feat: pos-list provides resources to descendants

--- elements/src/components/pos-list/pos-list.tsx
+++ elements/src/components/pos-list/pos-list.tsx
@@ -1,6 +1,12 @@
-import { Thing } from '@pod-os/core';
-import { Component, Element, Event, h, Prop, State } from '@stencil/core';
-import { ResourceAware, ResourceEventEmitter, subscribeResource } from '../events/ResourceAware';
+import { Thing, PodOS } from '@pod-os/core';
+import { Component, Element, Event, EventEmitter, h, Prop, State, Watch } from '@stencil/core';
+import { ResourceAware, ResourceEventEmitter, ResourceReceiver, subscribeResource } from '../events/ResourceAware';
+import { PodOsEventEmitter, subscribePodOs } from '../events/PodOsAware';
+
+interface SubscribeResourceEvent extends CustomEvent {
+  detail: ResourceReceiver;
+  currentTarget: HTMLElement;
+}
 
 @Component({
   tag: 'pos-list',
@@ -13,39 +19,74 @@ export class PosList implements ResourceAware {
   @Prop() rel: string;
 
   @Element() host: HTMLDivElement;
+  @State() os: PodOS;
   @State() error: string = null;
   @State() resource: Thing;
   @State() items: string[] = [];
   @State() templateNodeName: string;
   @State() templateString: string;
+  @State() provideQueue: SubscribeResourceEvent[] = [];
 
+  @Event({ eventName: 'pod-os:init' }) subscribePodOs: PodOsEventEmitter;
   @Event({ eventName: 'pod-os:resource' })
   subscribeResource: ResourceEventEmitter;
 
   componentWillLoad() {
+    subscribePodOs(this);
     subscribeResource(this);
-    let templateElement = this.host.querySelector("template")
+    let templateElement = this.host.querySelector('template');
     if (templateElement == null) {
-      this.error = "No template element found"
+      this.error = 'No template element found';
     } else if (templateElement.content.childElementCount != 1) {
-      this.error = "Template element should only have one child, e.g. li"
+      this.error = 'Template element should only have one child, e.g. li';
     } else {
-      this.templateNodeName = templateElement.content.firstElementChild.nodeName.toLowerCase()
-      this.templateString = templateElement.content.firstElementChild.innerHTML
+      this.templateNodeName = templateElement.content.firstElementChild.nodeName.toLowerCase();
+      this.templateString = templateElement.content.firstElementChild.innerHTML;
     }
   }
 
-  receiveResource = (resource: Thing) => {
-    this.items = []
-    if (this.rel) this.items = resource.relations().filter(relation => relation.predicate == this.rel).flatMap(relation => relation.uris);
+  receivePodOs = async (os: PodOS) => {
+    this.os = os;
   };
 
+  receiveResource = (resource: Thing) => {
+    this.items = [];
+    if (this.rel)
+      this.items = resource
+        .relations()
+        .filter(relation => relation.predicate == this.rel)
+        .flatMap(relation => relation.uris);
+  };
+
+  @Watch('os')
+  private async provideResources() {
+    while (this.provideQueue.length > 0) {
+      let ev = this.provideQueue.pop();
+      let resourceUri = ev.currentTarget.getAttribute('about');
+      let resource = this.os.store.get(resourceUri);
+      if (resource) {
+        ev.detail(resource);
+      } else {
+        this.provideQueue.push(ev);
+      }
+    }
+  }
+
+  async provideResource(event: SubscribeResourceEvent) {
+    event.stopPropagation();
+    this.provideQueue.push(event);
+    this.provideResources();
+  }
+
   render() {
-    if (this.error) return this.error
-    const elems = this.items.map(() => (
-      <this.templateNodeName innerHTML={this.templateString}></this.templateNodeName>
+    if (this.error) return this.error;
+    const elems = this.items.map(it => (
+      <this.templateNodeName
+        innerHTML={this.templateString}
+        about={it}
+        onPod-os:resource={ev => this.provideResource(ev)}
+      ></this.templateNodeName>
     ));
     return this.items.length > 0 ? elems : null;
   }
 }
-
