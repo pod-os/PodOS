460fb12107eda666efca174b0500cce261f9a118
Angelo Veltens <angelo+github.com@veltens.org>
2025-05-26 21:42:29 +0200

    Revert "refactor: use the provided helper function to setup the PodOS Browser service worker"
    This reverts commit 7ea416a896994cdaefd5514b8ae97da72634e398.
    
    Reason: esm service worker only works in Chrome

--- apps/service-worker.js
+++ apps/service-worker.js
@@ -1,7 +1,68 @@
-import { setupServiceWorker } from "https://cdn.jsdelivr.net/npm/@pod-os/elements@${POD_OS_ELEMENTS_VERSION}/dist/esm/index.js";
-
 const CACHE_NAME = "pod-os-static-${POD_OS_ELEMENTS_VERSION}";
 const CDN_URL_PATTERN = "https://cdn.jsdelivr.net/npm/@pod-os/";
-const INITIALLY_CACHED_FILES = ["/", "./index.html", "./manifest.json"];
 
-setupServiceWorker(self, INITIALLY_CACHED_FILES, CACHE_NAME, CDN_URL_PATTERN);
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open(CACHE_NAME);
+  await cache.addAll(resources);
+};
+
+const putInCache = async (request, response) => {
+  const cache = await caches.open(CACHE_NAME);
+  await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request }) => {
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+
+  const responseFromNetwork = await fetch(request.clone());
+  if (request.url.startsWith(CDN_URL_PATTERN)) {
+    putInCache(request, responseFromNetwork.clone());
+  }
+
+  return responseFromNetwork;
+};
+
+self.addEventListener("activate", (event) => {
+  console.log("Service worker activated");
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => {
+            console.log("Deleting old cache:", name);
+            return caches.delete(name);
+          }),
+      );
+    }),
+  );
+});
+
+self.addEventListener("install", (event) => {
+  console.log("ServiceWorker installed");
+  event.waitUntil(
+    addResourcesToCache([
+      "/",
+      "./index.html",
+      "./manifest.json",
+      "./favicon-32x32.png",
+    ]),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    console.log("Navigating to cached home page");
+    event.respondWith(caches.match("/"));
+    return;
+  }
+
+  event.respondWith(
+    cacheFirst({
+      request: event.request,
+    }),
+  );
+});
