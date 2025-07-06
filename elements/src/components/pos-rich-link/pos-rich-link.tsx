4e6bbe0fd6c5de6e9b1a4f9542510a42f404c33b
jg10 <181532694+jg10-mastodon-social@users.noreply.github.com>
2025-07-06 21:05:19 +1000

    feat(pos-rich-link): receive resource if uri is not present

--- elements/src/components/pos-rich-link/pos-rich-link.tsx
+++ elements/src/components/pos-rich-link/pos-rich-link.tsx
@@ -1,32 +1,58 @@
-import { Component, Event, EventEmitter, h, Prop } from '@stencil/core';
+import { Thing } from '@pod-os/core';
+import { Component, Event, EventEmitter, h, Prop, State } from '@stencil/core';
+import { ResourceAware, ResourceEventEmitter, subscribeResource } from '../events/ResourceAware';
 
 @Component({
   tag: 'pos-rich-link',
   shadow: true,
   styleUrl: 'pos-rich-link.css',
 })
-export class PosRichLink {
-  @Prop() uri: string;
+export class PosRichLink implements ResourceAware {
+  @Prop() uri?: string;
 
   @Event({ eventName: 'pod-os:link' }) linkEmitter: EventEmitter;
 
+  @Event({ eventName: 'pod-os:resource' })
+  subscribeResource: ResourceEventEmitter;
+
+  @State() resource?: Thing;
+
+  componentWillLoad() {
+    if (!this.uri) subscribeResource(this);
+  }
+
+  receiveResource = (resource: Thing) => {
+    this.resource = resource;
+  };
+
   render() {
-    return (
-      <pos-resource lazy={true} uri={this.uri}>
-        <p class="content">
-          <a
-            href={this.uri}
-            onClick={e => {
-              e.preventDefault();
-              this.linkEmitter.emit(this.uri);
-            }}
-          >
-            <pos-label />
-          </a>
-          <span class="url">{new URL(this.uri).host}</span>
-          <pos-description />
-        </p>
-      </pos-resource>
+    const uri = this.uri || this.resource?.uri;
+    if (!uri) return null;
+
+    const content = (
+      <p class="content">
+        <a
+          href={uri}
+          onClick={e => {
+            e.preventDefault();
+            this.linkEmitter.emit(uri);
+          }}
+        >
+          <pos-label />
+        </a>
+        <span class="url">{new URL(uri).host}</span>
+        <pos-description />
+      </p>
     );
+
+    if (this.resource) {
+      return content;
+    } else if (this.uri) {
+      return (
+        <pos-resource lazy={true} uri={this.uri}>
+          {content}
+        </pos-resource>
+      );
+    }
   }
 }
