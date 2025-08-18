4e12eb92e8f121c60545d7ed03cffc7b95282d7e
Angelo Veltens <angelo+github.com@veltens.org>
2025-08-18 20:59:38 +0200

    test(e2e): use correct idp URL respecting the trailing slash that is configured in the user profile

--- apps/tests/actions/signIn.ts
+++ apps/tests/actions/signIn.ts
@@ -9,14 +9,14 @@ export interface Credentials {
 export async function signIn(
   page: Page,
   credentials: Credentials,
-  idp: string = "http://localhost:4000",
+  idp: string = "http://localhost:4000/",
 ) {
   const originalLocation = page.url();
   const loginButton = page.locator("pos-login").getByRole("button");
   await loginButton.click();
 
   const idpUrl = page.getByLabel("Please enter your Identity Provider");
-  await idpUrl.fill("http://localhost:4000");
+  await idpUrl.fill(idp);
   await idpUrl.press("Enter");
 
   await expect(page).toHaveURL(
