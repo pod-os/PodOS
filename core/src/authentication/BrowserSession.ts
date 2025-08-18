947207fd9f169852bf33a3bf2bc5dacc6037753f
Angelo Veltens <angelo+github.com@veltens.org>
2025-08-18 21:55:13 +0200

    refactor: move BrowserSession to own file

---
+++ core/src/authentication/BrowserSession.ts
@@ -0,0 +1,61 @@
+import { Session } from "@uvdsl/solid-oidc-client-browser";
+import { BehaviorSubject } from "rxjs";
+import { AuthenticatedFetch, PodOsSession, SessionInfo } from "./index";
+
+export class BrowserSession implements PodOsSession {
+  private readonly session: Session;
+  private onSessionRestoreCallback: (url: string) => void = () => {};
+  private readonly _authenticatedFetch: AuthenticatedFetch;
+
+  private readonly sessionInfo$: BehaviorSubject<SessionInfo> =
+    new BehaviorSubject<SessionInfo>({
+      isLoggedIn: false,
+      webId: undefined,
+    });
+
+  get authenticatedFetch(): (
+    url: RequestInfo,
+    init?: RequestInit | undefined,
+  ) => Promise<Response> {
+    return this._authenticatedFetch;
+  }
+
+  constructor() {
+    this.session = new Session();
+    this._authenticatedFetch = this.session.authFetch.bind(this.session);
+  }
+
+  async handleIncomingRedirect(restorePreviousSession = false) {
+    await this.session.handleRedirectFromLogin();
+    if (restorePreviousSession) {
+      await this.session.restore();
+      if (this.session.isActive) {
+        this.onSessionRestoreCallback(window.location.href);
+      }
+    }
+    this.sessionInfo$.next({
+      isLoggedIn: this.session.isActive,
+      webId: this.session.webId,
+    });
+  }
+
+  async login(oidcIssuer: string) {
+    return this.session.login(oidcIssuer, window.location.href);
+  }
+
+  async logout() {
+    await this.session.logout();
+    this.sessionInfo$.next({
+      isLoggedIn: false,
+      webId: undefined,
+    });
+  }
+
+  observeSession(): BehaviorSubject<SessionInfo> {
+    return this.sessionInfo$;
+  }
+
+  onSessionRestore(callback: (url: string) => void) {
+    this.onSessionRestoreCallback = callback;
+  }
+}
