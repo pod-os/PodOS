7c4752521677f55e7a221a08dc1c49b212fbfa64
Angelo Veltens <angelo+github.com@veltens.org>
2025-08-19 08:05:18 +0200

    test: BrowserSession

--- core/src/authentication/BrowserSession.spec.ts
+++ core/src/authentication/BrowserSession.spec.ts
@@ -1,10 +1,10 @@
 import { BrowserSession } from "./BrowserSession";
+import { Session } from "@uvdsl/solid-oidc-client-browser";
+import { first, firstValueFrom } from "rxjs";
 
 jest.mock("@uvdsl/solid-oidc-client-browser", () => ({
   Session: jest.fn(),
 }));
-
-import { Session } from "@uvdsl/solid-oidc-client-browser";
 
 describe("BrowserSession", () => {
   beforeEach(() => {
@@ -41,6 +41,109 @@ describe("BrowserSession", () => {
       expect(session.authenticatedFetch("irrelevant")).toEqual(
         "internal-value",
       );
+    });
+  });
+
+  describe("handleIncomingRedirect", () => {
+    let session: BrowserSession;
+    let handleRedirectFromLogin: jest.Mock;
+    let restore: jest.Mock;
+    beforeEach(() => {
+      handleRedirectFromLogin = jest.fn();
+      restore = jest.fn();
+      (Session as jest.Mock).mockImplementation(() => {
+        return {
+          authFetch: jest.fn(),
+          isActive: true,
+          webId: "http://pod.test/alice#me",
+          handleRedirectFromLogin,
+          restore,
+        };
+      });
+      session = new BrowserSession();
+    });
+    it("handles redirect from login", async () => {
+      await session.handleIncomingRedirect();
+      expect(handleRedirectFromLogin).toHaveBeenCalled();
+      const result = await firstValueFrom(session.observeSession());
+      expect(result).toEqual({
+        isLoggedIn: true,
+        webId: "http://pod.test/alice#me",
+      });
+    });
+    describe("session restore", () => {
+      let sessionRestoreCallback: jest.Mock;
+      beforeEach(() => {
+        sessionRestoreCallback = jest.fn();
+        session.onSessionRestore(sessionRestoreCallback);
+      });
+
+      it("does not restore session by default", async () => {
+        await session.handleIncomingRedirect();
+        expect(restore).not.toHaveBeenCalled();
+        expect(sessionRestoreCallback).not.toHaveBeenCalled();
+      });
+
+      it("restores session, if explicitly configured", async () => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (global as any).window = {
+          location: {
+            href: "https://current.test",
+          },
+        };
+        const sessionRestoreCallback = jest.fn();
+        session.onSessionRestore(sessionRestoreCallback);
+        await session.handleIncomingRedirect(true);
+        expect(restore).toHaveBeenCalled();
+        expect(sessionRestoreCallback).toHaveBeenCalledWith(
+          "https://current.test",
+        );
+      });
+    });
+  });
+
+  describe("login", () => {
+    it("logs in with given idp and current location as redirect url", () => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (global as any).window = {
+        location: {
+          href: "https://current.test",
+        },
+      };
+      const login = jest.fn();
+      (Session as jest.Mock).mockImplementation(() => {
+        return {
+          authFetch: jest.fn(),
+          login,
+        };
+      });
+      const session = new BrowserSession();
+      session.login("https://pod.test/");
+      expect(login).toHaveBeenCalledWith(
+        "https://pod.test/",
+        "https://current.test",
+      );
+    });
+  });
+
+  describe("logout", () => {
+    it("logs out", async () => {
+      const logout = jest.fn();
+      (Session as jest.Mock).mockImplementation(() => {
+        return {
+          authFetch: jest.fn(),
+          isActive: true,
+          webId: "http://pod.test/alice#me",
+          logout,
+        };
+      });
+      const session = new BrowserSession();
+      await session.logout();
+      expect(logout).toHaveBeenCalled();
+      const sessionInfo = await firstValueFrom(session.observeSession());
+      expect(sessionInfo).toEqual({
+        isLoggedIn: false,
+      });
     });
   });
 });
