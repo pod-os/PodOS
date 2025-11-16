d91f3c9a91ed0b9e0c319e28026849c0403b1439
jg10 <181532694+jg10-mastodon-social@users.noreply.github.com>
2025-11-16 17:53:14 +1100

    feat(pos-list): support if-typeof attribute

--- elements/src/components/pos-list/pos-list.tsx
+++ elements/src/components/pos-list/pos-list.tsx
@@ -1,16 +1,22 @@
-import { Thing } from '@pod-os/core';
+import { PodOS, Thing } from '@pod-os/core';
 import { Component, Element, Event, h, Prop, State } from '@stencil/core';
 import { ResourceAware, ResourceEventEmitter, subscribeResource } from '../events/ResourceAware';
+import { PodOsAware, PodOsEventEmitter, subscribePodOs } from '../events/PodOsAware';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   tag: 'pos-list',
   shadow: false,
 })
-export class PosList implements ResourceAware {
+export class PosList implements PodOsAware, ResourceAware {
   /**
    * URI of the predicate to follow
    */
   @Prop() rel: string;
+  /**
+   * URI of a class for which instances will be listed
+   */
+  @Prop() ifTypeof: string;
   /**
    * Whether listed resources should be fetched before being displayed
    */
@@ -18,15 +24,22 @@ export class PosList implements ResourceAware {
 
   @Element() host: HTMLElement;
   @State() error: string = null;
+  @State() os: PodOS;
   @State() resource: Thing;
   @State() items: string[] = [];
   @State() templateString: string;
 
   @Event({ eventName: 'pod-os:resource' })
   subscribeResource: ResourceEventEmitter;
+  @Event({ eventName: 'pod-os:init' })
+  subscribePodOs: PodOsEventEmitter;
+
+  private readonly disconnected$ = new Subject<void>();
 
   componentWillLoad() {
-    subscribeResource(this);
+    if (this.rel) subscribeResource(this);
+    if (this.ifTypeof) subscribePodOs(this);
+
     const templateElement = this.host.querySelector('template');
     if (templateElement == null) {
       this.error = 'No template element found';
@@ -40,11 +53,23 @@ export class PosList implements ResourceAware {
     if (this.rel) this.items = resource.relations(this.rel).flatMap(relation => relation.uris);
   };
 
+  receivePodOs = async (os: PodOS) => {
+    os.store
+      .observeFindMembers(this.ifTypeof)
+      .pipe(takeUntil(this.disconnected$))
+      .subscribe(value => (this.items = value));
+  };
+
   render() {
     if (this.error) return this.error;
     const elems = this.items.map(it => (
       <pos-resource uri={it} lazy={!this.fetch} innerHTML={this.templateString} about={it}></pos-resource>
     ));
     return this.items.length > 0 ? elems : null;
+  }
+
+  disconnectedCallback() {
+    this.disconnected$.next();
+    this.disconnected$.unsubscribe();
   }
 }
