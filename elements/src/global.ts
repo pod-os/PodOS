c74f663b77f88d8d7c0bcdd71b49936c82a7bdda
Angelo Veltens <angelo+github.com@veltens.org>
2025-11-16 21:00:42 +0100

    fix(elements): remove ionic import from global script and wrap in default function as recommended by stencil docs

--- elements/src/global.ts
+++ elements/src/global.ts
@@ -1,15 +1,15 @@
-import '@ionic/core';
-
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
-
-setBasePath(new URL('./shoelace', import.meta.url).toString());
-
-const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
 function updateTheme(e) {
   document.documentElement.classList.toggle('sl-theme-dark', e.matches);
 }
 
-updateTheme(mediaQuery);
+export default function () {
+  setBasePath(new URL('./shoelace', import.meta.url).toString());
 
-mediaQuery.addEventListener('change', updateTheme);
+  const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+  updateTheme(mediaQuery);
+
+  mediaQuery.addEventListener('change', updateTheme);
+}
