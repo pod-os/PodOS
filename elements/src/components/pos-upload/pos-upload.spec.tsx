9b372ef6988dc43787ae71057305f3fb637b5e0b
Angelo Veltens <angelo+github.com@veltens.org>
2025-11-17 20:32:36 +0100

    feat(elements): style file upload with drop zone

--- elements/src/components/pos-upload/pos-upload.spec.tsx
+++ elements/src/components/pos-upload/pos-upload.spec.tsx
@@ -2,16 +2,47 @@ import { newSpecPage } from '@stencil/core/testing';
 import { PosUpload } from './pos-upload';
 
 describe('pos-upload', () => {
-  it('renders', async () => {
-    const page = await newSpecPage({
+  let page;
+  let input: HTMLInputElement;
+
+  beforeEach(async () => {
+    page = await newSpecPage({
       components: [PosUpload],
       html: `<pos-upload />`,
+      supportsShadowDom: false,
     });
+    input = page.root?.querySelector('input');
+  });
 
+  it('renders', () => {
     expect(page.root).toEqualHtml(`
       <pos-upload>
-        <input type="file" />
+        <form>
+          <input accept="image/*" multiple="" type="file">
+        </form>
       </pos-upload>
     `);
+  });
+
+  it('adds dragover class to input on dragenter', async () => {
+    input.dispatchEvent(new Event('dragenter'));
+    await page.waitForChanges();
+    expect(input).toHaveClass('dragover');
+  });
+
+  it('removes dragover class from input on dragleave', async () => {
+    input.dispatchEvent(new Event('dragenter'));
+    await page.waitForChanges();
+    input.dispatchEvent(new Event('dragleave'));
+    await page.waitForChanges();
+    expect(input).not.toHaveClass('dragover');
+  });
+
+  it('removes dragover class from input on drop', async () => {
+    input.dispatchEvent(new Event('dragenter'));
+    await page.waitForChanges();
+    input.dispatchEvent(new Event('drop'));
+    await page.waitForChanges();
+    expect(input).not.toHaveClass('dragover');
   });
 });
--- elements/src/components/pos-upload/pos-upload.tsx
+++ elements/src/components/pos-upload/pos-upload.tsx
@@ -1,10 +1,28 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, State } from '@stencil/core';
 
 @Component({
   tag: 'pos-upload',
+  styleUrl: 'pos-upload.css',
+  shadow: true,
 })
 export class PosUpload {
+  @State() dragover: boolean = false;
+
   render() {
-    return <input type="file" />;
+    return (
+      <form>
+        <input
+          class={{
+            dragover: this.dragover,
+          }}
+          type="file"
+          multiple
+          accept="image/*"
+          onDragEnter={() => (this.dragover = true)}
+          onDragLeave={() => (this.dragover = false)}
+          onDrop={() => (this.dragover = false)}
+        ></input>
+      </form>
+    );
   }
 }
