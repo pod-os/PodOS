43279200e2134ed1030e3eb186cbccb70a848fc2
Angelo Veltens <angelo+github.com@veltens.org>
2025-11-18 20:48:59 +0100

    feat(core): implement picture upload to thing's container
    - Add test for uploading picture files to a thing's container
    - Implement uploadAndAddPicture method that derives container URI from thing
    - Upload picture file using FileFetcher.createNewFile

--- core/src/picture/PictureGateway.ts
+++ core/src/picture/PictureGateway.ts
@@ -3,6 +3,8 @@ import { Thing } from "../thing";
 import { Store } from "../Store";
 import { FileFetcher } from "../files/FileFetcher";
 import { HttpProblem, NetworkProblem } from "../problems";
+import { LdpContainer } from "../ldp-container";
+import { graph } from "rdflib";
 
 export class PictureGateway {
   constructor(
@@ -23,7 +25,12 @@ export class PictureGateway {
     thing: Thing,
     pictureFile: Blob,
   ): ResultAsync<UploadedPicture, HttpProblem | NetworkProblem> {
-    throw new Error("Not implemented");
+    const containerUri = thing.uri.substring(0, thing.uri.lastIndexOf("/") + 1);
+    const container = new LdpContainer(containerUri, graph(), true);
+
+    return this.fileFetcher
+      .createNewFile(container, "picture.png")
+      .map((file) => ({ url: file.url }));
   }
 }
 
