f7ad347739aef8c7a695312e98229f6a2fc11b91
Angelo Veltens <angelo+github.com@veltens.org>
2025-12-01 18:50:25 +0100

    refactor: use ProfileQuery from rdflib-utils to determine the preferences file

--- core/src/profile/WebIdProfile.ts
+++ core/src/profile/WebIdProfile.ts
@@ -1,28 +1,26 @@
 import { IndexedFormula, sym } from "rdflib";
 import { Thing } from "../thing";
+import { ProfileQuery } from "@solid-data-modules/rdflib-utils";
 
 /**
- * Allows to find things related to the WebID and their profile document
+ * Allows finding things related to the WebID and their profile document
  */
 export class WebIdProfile extends Thing {
+  private profileQuery: ProfileQuery;
   constructor(
     readonly webId: string,
     readonly store: IndexedFormula,
     readonly editable: boolean = false,
   ) {
     super(webId, store, editable);
+    this.profileQuery = new ProfileQuery(sym(this.webId), this.store);
   }
 
   /**
-   * Returns te URI of the preferences document
+   * Returns the URI of the preferences document
    */
   getPreferencesFile() {
-    return this.store.anyValue(
-      sym(this.webId),
-      sym("http://www.w3.org/ns/pim/space#preferencesFile"),
-      undefined,
-      sym(this.webId).doc(),
-    );
+    return this.profileQuery.queryPreferencesFile()?.value;
   }
 
   /**
