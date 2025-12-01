a561aac3effaf2026f24d552f1ff2fd6536fac85
Angelo Veltens <angelo+github.com@veltens.org>
2025-12-01 21:51:32 +0100

    refactor(ProfileGateway): specify return type explicitly

--- core/src/profile/ProfileGateway.ts
+++ core/src/profile/ProfileGateway.ts
@@ -3,7 +3,7 @@ import { Store } from "../Store";
 
 export class ProfileGateway {
   constructor(private readonly store: Store) {}
-  async fetchProfile(webId: string) {
+  async fetchProfile(webId: string): Promise<WebIdProfile> {
     await this.store.fetch(webId);
     const profile: WebIdProfile = this.store.get(webId).assume(WebIdProfile);
     const preferences = profile.getPreferencesFile();
