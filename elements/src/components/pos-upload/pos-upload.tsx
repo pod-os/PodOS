3f5d940a7ef458368022f57e9ce3f64bfb4eb3a1
Angelo Veltens <angelo+github.com@veltens.org>
2025-12-26 19:51:11 +0100

    feat(pos-upload): show video source dropdown when using camera

--- elements/src/components/pos-upload/pos-upload.tsx
+++ elements/src/components/pos-upload/pos-upload.tsx
@@ -31,7 +31,7 @@ export class PosUpload {
         theme: 'auto',
       })
       .use(ImageEditor)
-      .use(Webcam, { modes: ['picture'] });
+      .use(Webcam, { modes: ['picture'], showVideoSourceDropdown: true });
     uppy.setOptions({
       restrictions: {
         allowedFileTypes: this.accept,
