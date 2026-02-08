0bdfc4daf2576945efc9590c0f82b4ca524d893e
jg10 <181532694+jg10-mastodon-social@users.noreply.github.com>
2026-02-08 17:36:21 +1100

    feat(core): Add LdpContainer.observeContains

--- core/src/ldp-container/LdpContainer.contains.spec.ts
+++ core/src/ldp-container/LdpContainer.contains.spec.ts
@@ -1,7 +1,8 @@
-import { graph, IndexedFormula, sym } from "rdflib";
+import { graph, IndexedFormula, quad, sym } from "rdflib";
 import { PodOsSession } from "../authentication";
-import { LdpContainer } from "./LdpContainer";
+import { ContainerContent, LdpContainer } from "./LdpContainer";
 import { Store } from "../Store";
+import { Observable, Subscription } from "rxjs";
 
 describe("LDP container", () => {
   describe("contains", () => {
@@ -84,6 +85,120 @@ describe("LDP container", () => {
           name: "c",
         },
       ]);
+    });
+  });
+
+  describe("observeContains", () => {
+    jest.useFakeTimers();
+    let internalStore: IndexedFormula,
+      store: Store,
+      subscriber: jest.Mock,
+      subscription: Subscription,
+      observable: Observable<ContainerContent[]>;
+
+    beforeEach(() => {
+      internalStore = graph();
+      internalStore.add(
+        sym("https://pod.test/container/"),
+        sym("http://www.w3.org/ns/ldp#contains"),
+        sym("https://pod.test/container/file"),
+        sym("https://pod.test/container/"),
+      );
+      store = new Store(
+        {} as PodOsSession,
+        undefined,
+        undefined,
+        internalStore,
+      );
+      subscriber = jest.fn();
+      const container = new LdpContainer("https://pod.test/container/", store);
+      observable = container.observeContains();
+      subscription = observable.subscribe(subscriber);
+    });
+
+    //To avoid memory leak
+    afterEach(() => {
+      subscription.unsubscribe();
+    });
+
+    it("pushes existing values immediately after debounce time", () => {
+      // before debounce time is complete
+      jest.advanceTimersByTime(100);
+      expect(subscriber).toHaveBeenCalledTimes(0);
+
+      // after debounce time
+      jest.advanceTimersByTime(150);
+      expect(subscriber).toHaveBeenCalledTimes(1);
+      expect(subscriber.mock.calls).toEqual([
+        [
+          [
+            {
+              uri: "https://pod.test/container/file",
+              name: "file",
+            },
+          ],
+        ],
+      ]);
+    });
+
+    it("pushes new values once per group of changes until unsubcribe, ignoring irrelevant changes", () => {
+      // wait for first push to settle
+      jest.advanceTimersByTime(250);
+      // then update container contents
+      internalStore.removeDocument(sym("https://pod.test/container/"));
+      internalStore.addAll([
+        quad(
+          sym("https://pod.test/container/"),
+          sym("http://www.w3.org/ns/ldp#contains"),
+          sym("https://pod.test/container/file-1"),
+          sym("https://pod.test/container/"),
+        ),
+        quad(
+          sym("https://pod.test/container/"),
+          sym("http://www.w3.org/ns/ldp#contains"),
+          sym("https://pod.test/container/file-2"),
+          sym("https://pod.test/container/"),
+        ),
+        quad(
+          sym("http://recipe.test/2"),
+          sym("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),
+          sym("http://recipe.test/RecipeClass"),
+        ),
+      ]);
+      jest.advanceTimersByTime(255);
+      expect(subscriber).toHaveBeenCalledTimes(2);
+      expect(subscriber.mock.calls).toEqual([
+        [
+          [
+            {
+              uri: "https://pod.test/container/file",
+              name: "file",
+            },
+          ],
+        ],
+        [
+          [
+            {
+              uri: "https://pod.test/container/file-1",
+              name: "file-1",
+            },
+            {
+              uri: "https://pod.test/container/file-2",
+              name: "file-2",
+            },
+          ],
+        ],
+      ]);
+
+      // Stop listening to ignore future changes
+      subscription.unsubscribe();
+      internalStore.add(
+        sym("https://pod.test/container/"),
+        sym("http://www.w3.org/ns/ldp#contains"),
+        sym("https://pod.test/container/file-ignored"),
+        sym("https://pod.test/container/"),
+      );
+      expect(subscriber).toHaveBeenCalledTimes(2);
     });
   });
 });
