4198bc6f8a8f2bd7785213f350f20814cf9a059f
jg10 <181532694+jg10-mastodon-social@users.noreply.github.com>
2026-02-16 22:24:41 +1100

    feat(Thing): add observeRelations

--- core/src/thing/Thing.relations.spec.ts
+++ core/src/thing/Thing.relations.spec.ts
@@ -1,4 +1,4 @@
-import { blankNode, graph, sym, IndexedFormula } from "rdflib";
+import { blankNode, graph, sym, IndexedFormula, quad } from "rdflib";
 import { PodOsSession } from "../authentication";
 import { Thing } from "./Thing";
 import { Store } from "../Store";
@@ -199,31 +199,187 @@ describe("Thing", function () {
       const result = it.relations();
       expect(result).toEqual([]);
     });
+
+    it("only follows the given predicate if provided", () => {
+      const uri = "https://jane.doe.example/container/file.ttl#fragment";
+      internalStore.add(
+        sym(uri),
+        sym("http://vocab.test/first"),
+        sym("https://pod.example/first"),
+      );
+      internalStore.add(
+        sym(uri),
+        sym("http://vocab.test/second"),
+        sym("https://pod.example/second"),
+      );
+      const it = new Thing(
+        "https://jane.doe.example/container/file.ttl#fragment",
+        store,
+      );
+      const result = it.relations("http://vocab.test/first");
+      expect(result).toEqual([
+        {
+          predicate: "http://vocab.test/first",
+          label: "first",
+          uris: ["https://pod.example/first"],
+        },
+      ]);
+    });
   });
 
-  it("only follows the given predicate if provided", () => {
-    const uri = "https://jane.doe.example/container/file.ttl#fragment";
-    internalStore.add(
-      sym(uri),
-      sym("http://vocab.test/first"),
-      sym("https://pod.example/first"),
-    );
-    internalStore.add(
-      sym(uri),
-      sym("http://vocab.test/second"),
-      sym("https://pod.example/second"),
-    );
-    const it = new Thing(
-      "https://jane.doe.example/container/file.ttl#fragment",
-      store,
-    );
-    const result = it.relations("http://vocab.test/first");
-    expect(result).toEqual([
-      {
-        predicate: "http://vocab.test/first",
-        label: "first",
-        uris: ["https://pod.example/first"],
-      },
-    ]);
+  describe("observeRelations", () => {
+    jest.useFakeTimers();
+    it("pushes existing relations immediately and changed relations in groups, until unsubscribe, unless irrelevant", () => {
+      const internalStore = graph();
+      const uri = "https://jane.doe.example/container/file.ttl#fragment";
+      internalStore.addAll([
+        quad(
+          sym(uri),
+          sym("http://vocab.test/first"),
+          sym("https://pod.example/first"),
+        ),
+        quad(
+          sym(uri),
+          sym("http://vocab.test/second"),
+          sym("https://pod.example/second"),
+        ),
+      ]);
+      const store = new Store(
+        {} as PodOsSession,
+        undefined,
+        undefined,
+        internalStore,
+      );
+      const subscriber = jest.fn();
+      const thing = new Thing(uri, store);
+      const relationsSpy = jest.spyOn(thing, "relations");
+
+      const observable = thing.observeRelations();
+      const subscription = observable.subscribe(subscriber);
+
+      // Existing value
+      expect(subscriber).toHaveBeenCalledTimes(1);
+      expect(relationsSpy).toHaveBeenCalledTimes(1);
+      // Irrelevant statement about another resource
+      internalStore.add(
+        sym("http://example.com/other-resource"),
+        sym("http://vocab.test/first"),
+        sym("https://pod.example/first"),
+      );
+      jest.advanceTimersByTime(250);
+      expect(subscriber).toHaveBeenCalledTimes(1);
+      expect(relationsSpy).toHaveBeenCalledTimes(1);
+      // Changed by removal
+      internalStore.removeStatement(
+        quad(
+          sym(uri),
+          sym("http://vocab.test/second"),
+          sym("https://pod.example/second"),
+        ),
+      );
+      jest.advanceTimersByTime(250);
+      expect(subscriber).toHaveBeenCalledTimes(2);
+      expect(relationsSpy).toHaveBeenCalledTimes(2);
+      // Changed by added object
+      internalStore.add(
+        quad(
+          sym(uri),
+          sym("http://vocab.test/first"),
+          sym("https://pod.example/first-2"),
+        ),
+      );
+      jest.advanceTimersByTime(250);
+      expect(subscriber).toHaveBeenCalledTimes(3);
+      expect(relationsSpy).toHaveBeenCalledTimes(3);
+      // Changed by removing and adding group of statements
+      internalStore.removeStatement(
+        quad(
+          sym(uri),
+          sym("http://vocab.test/first"),
+          sym("https://pod.example/first-2"),
+        ),
+      );
+      internalStore.removeStatement(
+        quad(
+          sym(uri),
+          sym("http://vocab.test/first"),
+          sym("https://pod.example/first"),
+        ),
+      );
+      internalStore.addAll([
+        quad(
+          sym(uri),
+          sym("http://vocab.test/second"),
+          sym("https://pod.example/second"),
+        ),
+        quad(
+          sym(uri),
+          sym("http://vocab.test/second"),
+          sym("https://pod.example/second-1"),
+        ),
+      ]);
+      jest.advanceTimersByTime(250);
+      expect(subscriber).toHaveBeenCalledTimes(4);
+      expect(relationsSpy).toHaveBeenCalledTimes(4);
+      // Stop listening to ignore future changes
+      subscription.unsubscribe();
+      internalStore.add(
+        sym(uri),
+        sym("http://vocab.test/third"),
+        sym("https://pod.example/third"),
+      );
+      jest.advanceTimersByTime(250);
+      expect(subscriber).toHaveBeenCalledTimes(4);
+      expect(relationsSpy).toHaveBeenCalledTimes(4);
+      expect(subscriber.mock.calls).toEqual([
+        [
+          [
+            {
+              predicate: "http://vocab.test/first",
+              label: "first",
+              uris: ["https://pod.example/first"],
+            },
+            {
+              predicate: "http://vocab.test/second",
+              label: "second",
+              uris: ["https://pod.example/second"],
+            },
+          ],
+        ],
+        [
+          [
+            {
+              predicate: "http://vocab.test/first",
+              label: "first",
+              uris: ["https://pod.example/first"],
+            },
+          ],
+        ],
+        [
+          [
+            {
+              predicate: "http://vocab.test/first",
+              label: "first",
+              uris: [
+                "https://pod.example/first",
+                "https://pod.example/first-2",
+              ],
+            },
+          ],
+        ],
+        [
+          [
+            {
+              predicate: "http://vocab.test/second",
+              label: "second",
+              uris: [
+                "https://pod.example/second",
+                "https://pod.example/second-1",
+              ],
+            },
+          ],
+        ],
+      ]);
+    });
   });
 });
