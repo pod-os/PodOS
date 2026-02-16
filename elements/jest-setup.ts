fe3988a424e2166dc4f84d6430f4fe8b17844a7a
Angelo Veltens <angelo+github.com@veltens.org>
2026-02-16 20:12:29 +0100

    feat(pos-share): dummy contents for the share menu

--- elements/jest-setup.ts
+++ elements/jest-setup.ts
@@ -4,3 +4,6 @@ jest.mock('@shoelace-style/shoelace/dist/components/icon/icon.js', () => ({}));
 jest.mock('@shoelace-style/shoelace/dist/components/progress-bar/progress-bar.js', () => ({}));
 jest.mock('@shoelace-style/shoelace/dist/components/skeleton/skeleton.js', () => ({}));
 jest.mock('@shoelace-style/shoelace/dist/components/tooltip/tooltip.js', () => ({}));
+jest.mock('@shoelace-style/shoelace/dist/components/menu/menu.js', () => ({}));
+jest.mock('@shoelace-style/shoelace/dist/components/menu-item/menu-item.js', () => ({}));
+jest.mock('@shoelace-style/shoelace/dist/components/divider/divider.js', () => ({}));
