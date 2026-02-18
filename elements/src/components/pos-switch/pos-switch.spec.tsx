8266c1d44fbebf803f42b87085987d2d642251ee
jg10 <181532694+jg10-mastodon-social@users.noreply.github.com>
2026-02-18 22:29:09 +1100

    fix(pos-switch): reset when resource changes

--- elements/src/components/pos-switch/pos-switch.spec.tsx
+++ elements/src/components/pos-switch/pos-switch.spec.tsx
@@ -299,11 +299,56 @@ describe('pos-switch', () => {
       },
     ]);
     await page.waitForChanges();
-    console.log(page.root?.innerHTML);
     expect(page.root?.innerHTML).toEqualHtml(`
         <div>Recipe.</div>
         <div>Image.</div>
         <div>Recipe list.</div>
         `);
+  });
+
+  it('resets and updates when resource is changed', async () => {
+    const page = await newSpecPage({
+      components: [PosSwitch],
+      html: `
+      <pos-switch>
+        <pos-case if-typeof="http://schema.org/Recipe">
+          <template>Recipe.</template>
+        </pos-case>
+        <pos-case if-typeof="http://schema.org/Video">
+          <template>Video.</template>
+        </pos-case>
+      </pos-switch>`,
+    });
+    const observedTypes$ = new Subject<RdfType[]>();
+    const thing = {
+      uri: 'https://pod.example/recipe1',
+      observeTypes: () => observedTypes$,
+    };
+    page.rootInstance.receiveResource(thing);
+    observedTypes$.next([
+      {
+        label: 'Recipe',
+        uri: 'http://schema.org/Recipe',
+      },
+    ]);
+    await page.waitForChanges();
+    expect(page.root?.innerText).toEqualText('Recipe.');
+    // new thing
+    const observedTypes2$ = new Subject<RdfType[]>();
+    const thing2 = {
+      uri: 'https://pod.example/video1',
+      observeTypes: () => observedTypes2$,
+    };
+    page.rootInstance.receiveResource(thing2);
+    await page.waitForChanges();
+    expect(page.root?.innerText).toEqualText('');
+    observedTypes2$.next([
+      {
+        label: 'Video',
+        uri: 'http://schema.org/Video',
+      },
+    ]);
+    await page.waitForChanges();
+    expect(page.root?.innerText).toEqualText('Video.');
   });
 });
