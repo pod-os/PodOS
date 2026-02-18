a382dac1f703720284e080a0cf4033fbe98aa0c0
Angelo Veltens <angelo+github.com@veltens.org>
2026-02-18 08:31:22 +0100

    refactor: add hard-coded app registration for well-known apps

--- core/src/open-with/proposeAppsFor.ts
+++ core/src/open-with/proposeAppsFor.ts
@@ -2,34 +2,38 @@ import { Thing } from "../thing";
 
 import { OpenWithApp } from "./types";
 
+const APPS: { [key: string]: OpenWithApp } = {
+  DATA_BROWSER: {
+    name: "Data Browser (SolidOS)",
+    appUrl: "https://solidos.github.io/mashlib/dist/browse.html",
+    uriParam: "uri",
+  },
+  PENNY: {
+    name: "Penny",
+    appUrl: "https://penny.vincenttunru.com/explore/",
+    uriParam: "url",
+  },
+  SOLID_FILE_MANAGER: {
+    name: "Solid File Manager",
+    appUrl: "https://otto-aa.github.io/solid-filemanager/",
+    uriParam: "url",
+  },
+};
+
+// TODO more apps
+const appRegistrations: { [key: string]: OpenWithApp[] } = {
+  "http://www.w3.org/ns/ldp#Container": [APPS.SOLID_FILE_MANAGER],
+};
+
 /**
  * Returns a list of apps that can be used to open a resource
  * @param uri - The URI of the resource to open
  */
 export function proposeAppsFor(thing: Thing): OpenWithApp[] {
-  const apps = [
-    {
-      name: "Data Browser (SolidOS)",
-      appUrl: "https://solidos.github.io/mashlib/dist/browse.html",
-      uriParam: "uri",
-    },
-    {
-      name: "Penny",
-      appUrl: "https://penny.vincenttunru.com/explore/",
-      uriParam: "url",
-    },
-  ];
-  if (
-    thing
-      .types()
-      .map((it) => it.uri)
-      .includes("http://www.w3.org/ns/ldp#Container")
-  ) {
-    apps.push({
-      name: "Solid File Manager",
-      appUrl: "https://otto-aa.github.io/solid-filemanager/",
-      uriParam: "url",
-    });
-  }
-  return apps;
+  const apps = [APPS.DATA_BROWSER, APPS.PENNY];
+  const appsForType = thing
+    .types()
+    .flatMap((it) => appRegistrations[it.uri])
+    .filter((it) => it !== undefined);
+  return [...apps, ...appsForType];
 }
