e668dfda89f91e1f75ef19ec8de1037cc3379797
jg10 <181532694+jg10-mastodon-social@users.noreply.github.com>
2026-02-28 22:49:09 +1100

    test(Thing): split up observeReverseRelations tests

--- core/src/thing/Thing.reverseRelations.spec.ts
+++ core/src/thing/Thing.reverseRelations.spec.ts
@@ -1,7 +1,8 @@
 import { graph, IndexedFormula, quad, sym } from "rdflib";
 import { PodOsSession } from "../authentication";
-import { Thing } from "./Thing";
+import { Relation, Thing } from "./Thing";
 import { Store } from "../Store";
+import { Observable, Subscription } from "rxjs";
 
 describe("Thing", function () {
   describe("reverse relations", () => {
@@ -176,9 +177,22 @@ describe("Thing", function () {
 
   describe("observeReverseRelations", () => {
     jest.useFakeTimers();
-    it("pushes existing reverse relations immediately and changed relations in groups, until unsubscribe, unless irrelevant", () => {
-      const internalStore = graph();
-      const uri = "https://jane.doe.example/container/file.ttl#fragment";
+
+    const mockSession = {} as unknown as PodOsSession;
+    let store: Store,
+      internalStore: IndexedFormula,
+      uri: string,
+      subscriber: jest.Mock,
+      thing: Thing,
+      reverseRelationsSpy: jest.SpyInstance,
+      observable: Observable<Relation[]>,
+      subscription: Subscription;
+
+    beforeEach(() => {
+      // Given a store with statements with a URI as object
+      internalStore = graph();
+      store = new Store(mockSession, undefined, undefined, internalStore);
+      uri = "https://jane.doe.example/container/file.ttl#fragment";
       internalStore.addAll([
         quad(
           sym("https://pod.example/first"),
@@ -191,93 +205,20 @@ describe("Thing", function () {
           sym(uri),
         ),
       ]);
-      const store = new Store(
-        {} as PodOsSession,
-        undefined,
-        undefined,
-        internalStore,
-      );
-      const subscriber = jest.fn();
-      const thing = new Thing(uri, store);
-      const reverseRelationsSpy = jest.spyOn(thing, "reverseRelations");
 
-      const observable = thing.observeReverseRelations();
-      const subscription = observable.subscribe(subscriber);
+      // and a Thing with a reverseRelations method
+      subscriber = jest.fn();
+      thing = new Thing(uri, store);
+      reverseRelationsSpy = jest.spyOn(thing, "reverseRelations");
 
-      // Existing value
+      // and a subscription to changes in reverse relations
+      observable = thing.observeReverseRelations();
+      subscription = observable.subscribe(subscriber);
+    });
+
+    it("pushes existing reverse relations immediately", () => {
       expect(subscriber).toHaveBeenCalledTimes(1);
       expect(reverseRelationsSpy).toHaveBeenCalledTimes(1);
-      // Irrelevant statement about another resource
-      internalStore.add(
-        sym("https://pod.example/first"),
-        sym("http://vocab.test/first"),
-        sym("http://example.com/other-resource"),
-      );
-      jest.advanceTimersByTime(250);
-      expect(subscriber).toHaveBeenCalledTimes(1);
-      expect(reverseRelationsSpy).toHaveBeenCalledTimes(1);
-      // Changed by removal
-      internalStore.removeStatement(
-        quad(
-          sym("https://pod.example/second"),
-          sym("http://vocab.test/second"),
-          sym(uri),
-        ),
-      );
-      jest.advanceTimersByTime(250);
-      expect(subscriber).toHaveBeenCalledTimes(2);
-      expect(reverseRelationsSpy).toHaveBeenCalledTimes(2);
-      // Changed by added object
-      internalStore.add(
-        quad(
-          sym("https://pod.example/first-2"),
-          sym("http://vocab.test/first"),
-          sym(uri),
-        ),
-      );
-      jest.advanceTimersByTime(250);
-      expect(subscriber).toHaveBeenCalledTimes(3);
-      expect(reverseRelationsSpy).toHaveBeenCalledTimes(3);
-      // Changed by removing and adding group of statements
-      internalStore.removeStatement(
-        quad(
-          sym("https://pod.example/first-2"),
-          sym("http://vocab.test/first"),
-          sym(uri),
-        ),
-      );
-      internalStore.removeStatement(
-        quad(
-          sym("https://pod.example/first"),
-          sym("http://vocab.test/first"),
-          sym(uri),
-        ),
-      );
-      internalStore.addAll([
-        quad(
-          sym("https://pod.example/second"),
-          sym("http://vocab.test/second"),
-          sym(uri),
-        ),
-        quad(
-          sym("https://pod.example/second-1"),
-          sym("http://vocab.test/second"),
-          sym(uri),
-        ),
-      ]);
-      jest.advanceTimersByTime(250);
-      expect(subscriber).toHaveBeenCalledTimes(4);
-      expect(reverseRelationsSpy).toHaveBeenCalledTimes(4);
-      // Stop listening to ignore future changes
-      subscription.unsubscribe();
-      internalStore.add(
-        sym("https://pod.example/third"),
-        sym("http://vocab.test/third"),
-        sym(uri),
-      );
-      jest.advanceTimersByTime(250);
-      expect(subscriber).toHaveBeenCalledTimes(4);
-      expect(reverseRelationsSpy).toHaveBeenCalledTimes(4);
       expect(subscriber.mock.calls).toEqual([
         [
           [
@@ -293,40 +234,117 @@ describe("Thing", function () {
             },
           ],
         ],
+      ]);
+    });
+
+    it("ignores irrelevant statements about other resources", () => {
+      internalStore.add(
+        sym("https://pod.example/first"),
+        sym("http://vocab.test/first"),
+        sym("http://example.com/other-resource"),
+      );
+      jest.advanceTimersByTime(250);
+      expect(subscriber).toHaveBeenCalledTimes(1);
+      expect(reverseRelationsSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it("updates after removals", () => {
+      internalStore.removeStatement(
+        quad(
+          sym("https://pod.example/second"),
+          sym("http://vocab.test/second"),
+          sym(uri),
+        ),
+      );
+      jest.advanceTimersByTime(250);
+      expect(subscriber).toHaveBeenCalledTimes(2);
+      expect(reverseRelationsSpy).toHaveBeenCalledTimes(2);
+      expect(subscriber.mock.lastCall).toEqual([
         [
-          [
-            {
-              predicate: "http://vocab.test/first",
-              label: "first",
-              uris: ["https://pod.example/first"],
-            },
-          ],
-        ],
-        [
-          [
-            {
-              predicate: "http://vocab.test/first",
-              label: "first",
-              uris: [
-                "https://pod.example/first",
-                "https://pod.example/first-2",
-              ],
-            },
-          ],
-        ],
-        [
-          [
-            {
-              predicate: "http://vocab.test/second",
-              label: "second",
-              uris: [
-                "https://pod.example/second",
-                "https://pod.example/second-1",
-              ],
-            },
-          ],
+          {
+            predicate: "http://vocab.test/first",
+            label: "first",
+            uris: ["https://pod.example/first"],
+          },
         ],
       ]);
+    });
+
+    it("updates after added subject", () => {
+      internalStore.add(
+        quad(
+          sym("https://pod.example/first-2"),
+          sym("http://vocab.test/first"),
+          sym(uri),
+        ),
+      );
+      jest.advanceTimersByTime(250);
+      expect(subscriber).toHaveBeenCalledTimes(2);
+      expect(reverseRelationsSpy).toHaveBeenCalledTimes(2);
+      expect(subscriber.mock.lastCall).toEqual([
+        [
+          {
+            predicate: "http://vocab.test/first",
+            label: "first",
+            uris: ["https://pod.example/first", "https://pod.example/first-2"],
+          },
+          {
+            predicate: "http://vocab.test/second",
+            label: "second",
+            uris: ["https://pod.example/second"],
+          },
+        ],
+      ]);
+    });
+
+    it("pushes changed relations in groups", () => {
+      internalStore.removeStatement(
+        quad(
+          sym("https://pod.example/first"),
+          sym("http://vocab.test/first"),
+          sym(uri),
+        ),
+      );
+      internalStore.addAll([
+        quad(
+          sym("https://pod.example/second-1"),
+          sym("http://vocab.test/second"),
+          sym(uri),
+        ),
+        quad(
+          sym("https://pod.example/second-2"),
+          sym("http://vocab.test/second"),
+          sym(uri),
+        ),
+      ]);
+      jest.advanceTimersByTime(250);
+      expect(subscriber).toHaveBeenCalledTimes(2);
+      expect(reverseRelationsSpy).toHaveBeenCalledTimes(2);
+      expect(subscriber.mock.lastCall).toEqual([
+        [
+          {
+            predicate: "http://vocab.test/second",
+            label: "second",
+            uris: [
+              "https://pod.example/second",
+              "https://pod.example/second-1",
+              "https://pod.example/second-2",
+            ],
+          },
+        ],
+      ]);
+    });
+
+    it("stops pushing after unsubscribe", () => {
+      subscription.unsubscribe();
+      internalStore.add(
+        sym("https://pod.example/third"),
+        sym("http://vocab.test/third"),
+        sym(uri),
+      );
+      jest.advanceTimersByTime(250);
+      expect(subscriber).toHaveBeenCalledTimes(1);
+      expect(reverseRelationsSpy).toHaveBeenCalledTimes(1);
     });
   });
 });
