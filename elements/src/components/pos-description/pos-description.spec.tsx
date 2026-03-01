07880b30d14b94e48c7238828a2f8179e47ca765
jg10 <181532694+jg10-mastodon-social@users.noreply.github.com>
2026-03-01 22:49:17 +1100

    feat(elements): pos-description uses observeDescription

--- elements/src/components/pos-description/pos-description.spec.tsx
+++ elements/src/components/pos-description/pos-description.spec.tsx
@@ -1,5 +1,6 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { PosDescription } from './pos-description';
+import { Subject } from 'rxjs';
 
 describe('pos-description', () => {
   it('is empty initially', async () => {
@@ -19,13 +20,44 @@ describe('pos-description', () => {
       components: [PosDescription],
       html: `<pos-description />`,
     });
-    await page.rootInstance.receiveResource({
-      description: () => 'Test Resource',
-    });
+    const observedDescription$ = new Subject<string>();
+    const resource = {
+      observeDescription: () => observedDescription$,
+    };
+    await page.rootInstance.receiveResource(resource);
+    observedDescription$.next('Test Resource');
     await page.waitForChanges();
     expect(page.root).toEqualHtml(`
       <pos-description>
         <mock:shadow-root>Test Resource</mock:shadow-root>
+      </pos-description>
+  `);
+  });
+
+  it('updates description when changes', async () => {
+    const page = await newSpecPage({
+      components: [PosDescription],
+      html: `<pos-description />`,
+    });
+    const observedDescription$ = new Subject<string>();
+    const resource = {
+      observeDescription: () => observedDescription$,
+    };
+    await page.rootInstance.receiveResource(resource);
+
+    observedDescription$.next('Test Resource');
+    await page.waitForChanges();
+    expect(page.root).toEqualHtml(`
+      <pos-description>
+        <mock:shadow-root>Test Resource</mock:shadow-root>
+      </pos-description>
+  `);
+
+    observedDescription$.next('Test Resource 2');
+    await page.waitForChanges();
+    expect(page.root).toEqualHtml(`
+      <pos-description>
+        <mock:shadow-root>Test Resource 2</mock:shadow-root>
       </pos-description>
   `);
   });
