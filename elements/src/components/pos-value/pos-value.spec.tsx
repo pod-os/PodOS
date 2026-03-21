15ed0dbfb817987b7eb08ad077d6cf9c069df127
jg10 <181532694+jg10-mastodon-social@users.noreply.github.com>
2026-03-21 11:48:36 +1100

    feat(pos-value): updates when data changes using observeAnyValue

--- elements/src/components/pos-value/pos-value.spec.tsx
+++ elements/src/components/pos-value/pos-value.spec.tsx
@@ -1,5 +1,6 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { PosValue } from './pos-value';
+import { ReplaySubject, Subject } from 'rxjs';
 
 describe('pos-value', () => {
   it('is empty initially', async () => {
@@ -19,13 +20,40 @@ describe('pos-value', () => {
       components: [PosValue],
       html: `<pos-value predicate="https://vocab.example/#term" />`,
     });
+    const observedValue$ = new ReplaySubject<string>();
+    observedValue$.next(`value of https://vocab.example/#term`);
     await page.rootInstance.receiveResource({
-      anyValue: uri => `value of ${uri}`,
+      observeAnyValue: (uri: string) => (uri === 'https://vocab.example/#term' ? observedValue$ : undefined),
     });
     await page.waitForChanges();
     expect(page.root).toEqualHtml(`
       <pos-value predicate="https://vocab.example/#term" />
         <mock:shadow-root>value of https://vocab.example/#term</mock:shadow-root>
+      </>
+  `);
+  });
+
+  it('updates property value when changed', async () => {
+    const page = await newSpecPage({
+      components: [PosValue],
+      html: `<pos-value predicate="https://vocab.example/#term" />`,
+    });
+    const observedValue$ = new Subject<string>();
+    await page.rootInstance.receiveResource({
+      observeAnyValue: (uri: string) => (uri === 'https://vocab.example/#term' ? observedValue$ : undefined),
+    });
+    observedValue$.next(`value of https://vocab.example/#term`);
+    await page.waitForChanges();
+    expect(page.root).toEqualHtml(`
+      <pos-value predicate="https://vocab.example/#term" />
+        <mock:shadow-root>value of https://vocab.example/#term</mock:shadow-root>
+      </>
+  `);
+    observedValue$.next(`new value of https://vocab.example/#term`);
+    await page.waitForChanges();
+    expect(page.root).toEqualHtml(`
+      <pos-value predicate="https://vocab.example/#term" />
+        <mock:shadow-root>new value of https://vocab.example/#term</mock:shadow-root>
       </>
   `);
   });
