c5b8a3986f5a2890a36c58aaf1c718afa4a9f273
jg10 <181532694+jg10-mastodon-social@users.noreply.github.com>
2026-03-21 11:48:36 +1100

    feat(pos-value): updates when data changes using observeAnyValue

--- elements/src/components/pos-value/pos-value.tsx
+++ elements/src/components/pos-value/pos-value.tsx
@@ -1,10 +1,12 @@
 import { Thing } from '@pod-os/core';
 import { Component, Event, Prop, State } from '@stencil/core';
 import { ResourceAware, ResourceEventEmitter, subscribeResource } from '../events/ResourceAware';
+import { Subject, takeUntil } from 'rxjs';
 
 /**
  * Shows a single value linked to the resource using the given predicate.
- * The value is determined by [Thing.anyValue()](https://pod-os.org/reference/core/classes/thing/#anyvalue)
+ * The value is determined by [Thing.observeAnyValue()](https://pod-os.org/reference/core/classes/thing/#observeanyvalue)
+ * and re-renders when data in the store changes
  */
 @Component({
   tag: 'pos-value',
@@ -15,20 +17,30 @@ export class PosValue implements ResourceAware {
    * URI of the predicate to get the value from
    */
   @Prop() predicate: string;
-  @State() resource: Thing;
+  @State() value: string;
 
   @Event({ eventName: 'pod-os:resource' })
   subscribeResource: ResourceEventEmitter;
+
+  private readonly disconnected$ = new Subject<void>();
 
   componentWillLoad() {
     subscribeResource(this);
   }
 
   receiveResource = (resource: Thing) => {
-    this.resource = resource;
+    resource
+      .observeAnyValue(this.predicate)
+      .pipe(takeUntil(this.disconnected$))
+      .subscribe(value => (this.value = value));
   };
 
   render() {
-    return this.resource ? this.resource.anyValue(this.predicate) : null;
+    return this.value ?? null;
+  }
+
+  disconnectedCallback() {
+    this.disconnected$.next();
+    this.disconnected$.complete();
   }
 }
