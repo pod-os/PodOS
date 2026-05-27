0f398fa596bc07ab964e4fac8b4109ae614a2d3f
Angelo <angelo+github.com@veltens.org>
2026-05-27 18:30:45 +0200

    chore(core): major upgrade to eslint 10

--- core/eslint.config.mjs
+++ core/eslint.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
 
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-  languageOptions: {
-    ecmaVersion: 2022,
-    sourceType: "module",
-    globals: {
-      console: "readonly",
+export default defineConfig(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        console: "readonly",
+      },
     },
-  },
-  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
-});
+  }
+);
