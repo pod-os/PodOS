f50f00547a820634543f0cdd7e6de27c4da527e8
Angelo <angelo+github.com@veltens.org>
2026-06-17 19:25:58 +0200

    test: migrate pos-container-contents tests to vitest

--- elements/src/components/pos-container-contents/pos-container-contents.vspec.tsx
+++ elements/src/components/pos-container-contents/pos-container-contents.vspec.tsx
@@ -1,15 +1,19 @@
+import { vi } from 'vitest';
+import { beforeEach, describe, expect, it, render, h } from '@stencil/vitest';
 import { fireEvent } from '@testing-library/dom';
 
-jest.mock('@pod-os/core', () => ({}));
+import './pos-container-contents';
 
-import { newSpecPage } from '@stencil/core/testing';
-
-import { PosContainerContents } from './pos-container-contents';
 import { Components, LdpContainer } from '../../components';
 import PosCreateNewContainerItem = Components.PosCreateNewContainerItem;
 import { pressKey } from '../../test/pressKey';
 import { Subject } from 'rxjs';
 import { ContainerContent, Thing } from '@pod-os/core';
+import { mockResource } from '../../test/mockResource';
+
+vi.mock('@pod-os/core', () => ({
+  LdpContainer: class {},
+}));
 
 describe('pos-container-contents', () => {
   let container: LdpContainer, observed$: Subject<ContainerContent[]>, resource: Thing;
@@ -27,24 +31,14 @@ describe('pos-container-contents', () => {
   });
 
   it('are empty initially', async () => {
-    const page = await newSpecPage({
-      components: [PosContainerContents],
-      html: `<pos-container-contents />`,
-    });
-    expect(page.root).toEqualHtml(`
-      <pos-container-contents>
-        <mock:shadow-root></mock:shadow-root>
-      </pos-container-contents>
-  `);
+    const page = await render(<pos-container-contents></pos-container-contents>);
+    expect(page.root).toBeEmptyDOMElement();
   });
 
   it('renders single file and a link to it', async () => {
-    const page = await newSpecPage({
-      components: [PosContainerContents],
-      html: `<pos-container-contents />`,
-      supportsShadowDom: false,
-    });
-    await page.rootInstance.receiveResource(resource);
+    mockResource(resource);
+    const page = await render(<pos-container-contents></pos-container-contents>);
+
     observed$.next([
       {
         uri: 'https://pod.test/container/file',
@@ -53,48 +47,38 @@ describe('pos-container-contents', () => {
     ]);
     await page.waitForChanges();
 
-    expect(page.root).toEqualHtml(`
-      <pos-container-contents>
+    expect(page.root.shadowRoot).toEqualHtml(`
         <pos-container-toolbar></pos-container-toolbar>
         <ul aria-label="Container contents">
           <li>
-            <pos-resource lazy="" uri="https://pod.test/container/file">
+            <pos-resource lazy uri="https://pod.test/container/file">
               <pos-container-item>
                 file
               </pos-container-item>
             </pos-resource>
           </li>
-        </ul>
-      </pos-container-contents>`);
+        </ul>`);
   });
 
   it('renders a note about container being empty', async () => {
-    const page = await newSpecPage({
-      components: [PosContainerContents],
-      html: `<pos-container-contents />`,
-    });
-    await page.rootInstance.receiveResource(resource);
+    mockResource(resource);
+    const page = await render(<pos-container-contents></pos-container-contents>);
+
     observed$.next([]);
     await page.waitForChanges();
 
-    expect(page.root).toEqualHtml(`<pos-container-contents>
-      <mock:shadow-root>
-        <pos-container-toolbar></pos-container-toolbar>
-        <p>
-          The container is empty
-        </p>
-      </mock:shadow-root>
-    </pos-container-contents>
- `);
+    expect(page.root.shadowRoot).toEqualHtml(`
+      <pos-container-toolbar></pos-container-toolbar>
+      <p>
+        The container is empty
+      </p>
+    `);
   });
 
   it('renders multiple contents and links to them, sorted alphabetically', async () => {
-    const page = await newSpecPage({
-      components: [PosContainerContents],
-      html: `<pos-container-contents />`,
-      supportsShadowDom: false,
-    });
-    await page.rootInstance.receiveResource(resource);
+    mockResource(resource);
+    const page = await render(<pos-container-contents></pos-container-contents>);
+
     observed$.next([
       {
         uri: 'https://pod.test/container/file',
@@ -111,44 +95,37 @@ describe('pos-container-contents', () => {
     ]);
     await page.waitForChanges();
 
-    expect(page.root).toEqualHtml(`
-      <pos-container-contents>
+    expect(page.root.shadowRoot).toEqualHtml(`
         <pos-container-toolbar></pos-container-toolbar>
         <ul aria-label="Container contents">
           <li>
-            <pos-resource lazy="" uri="https://pod.test/container/a-file-on-top-of-the-list">
+            <pos-resource lazy uri="https://pod.test/container/a-file-on-top-of-the-list">
               <pos-container-item>
                 a-file-on-top-of-the-list
               </pos-container-item>
             </pos-resource>
           </li>
           <li>
-            <pos-resource lazy="" uri="https://pod.test/container/file">
+            <pos-resource lazy uri="https://pod.test/container/file">
               <pos-container-item>
                 file
               </pos-container-item>
             </pos-resource>
           </li>
           <li>
-            <pos-resource lazy="" uri="https://pod.test/container/subdir/">
+            <pos-resource lazy uri="https://pod.test/container/subdir/">
               <pos-container-item>
                 subdir
               </pos-container-item>
             </pos-resource>
           </li>
         </ul>
-      </pos-container-contents>
     `);
   });
 
   it('re-renders when container contents in store change', async () => {
-    const page = await newSpecPage({
-      components: [PosContainerContents],
-      html: `<pos-container-contents />`,
-      supportsShadowDom: false,
-    });
-
-    await page.rootInstance.receiveResource(resource);
+    mockResource(resource);
+    const page = await render(<pos-container-contents></pos-container-contents>);
 
     observed$.next([
       {
@@ -158,19 +135,17 @@ describe('pos-container-contents', () => {
     ]);
     await page.waitForChanges();
 
-    expect(page.root).toEqualHtml(`
-      <pos-container-contents>
+    expect(page.root.shadowRoot).toEqualHtml(`
         <pos-container-toolbar></pos-container-toolbar>
         <ul aria-label="Container contents">
           <li>
-            <pos-resource lazy="" uri="https://pod.test/container/file">
+            <pos-resource lazy uri="https://pod.test/container/file">
               <pos-container-item>
                 file
               </pos-container-item>
             </pos-resource>
           </li>
-        </ul>
-      </pos-container-contents>`);
+        </ul>`);
 
     observed$.next([
       {
@@ -180,33 +155,26 @@ describe('pos-container-contents', () => {
     ]);
     await page.waitForChanges();
 
-    expect(page.root).toEqualHtml(`
-      <pos-container-contents>
+    expect(page.root.shadowRoot).toEqualHtml(`
         <pos-container-toolbar></pos-container-toolbar>
         <ul aria-label="Container contents">
           <li>
-            <pos-resource lazy="" uri="https://pod.test/container/file-1">
+            <pos-resource lazy uri="https://pod.test/container/file-1">
               <pos-container-item>
                 file-1
               </pos-container-item>
             </pos-resource>
           </li>
         </ul>
-      </pos-container-contents>`);
+    `);
   });
 
   describe('new files and folders', () => {
     let page: any;
     beforeEach(async () => {
       // and given a page with container contents
-      page = await newSpecPage({
-        components: [PosContainerContents],
-        html: `<pos-container-contents />`,
-        supportsShadowDom: false,
-      });
-
-      // and a container resource is available
-      await page.rootInstance.receiveResource(resource);
+      mockResource(resource);
+      page = await render(<pos-container-contents></pos-container-contents>);
 
       // as well as (empty) container contents
       observed$.next([]);
@@ -215,51 +183,47 @@ describe('pos-container-contents', () => {
 
     it('shows input to create a new file when event occurs', async () => {
       // when the toolbar fires a create-new-file event
-      const toolbar = page.root.querySelector('pos-container-toolbar');
+      const toolbar = page.root.shadowRoot!.querySelector('pos-container-toolbar')!;
       expect(toolbar).not.toBeNull();
       fireEvent(toolbar, new CustomEvent('pod-os:create-new-file', {}));
       await page.waitForChanges();
 
       // then the input to create a new file is shown
-      expect(page.root).toEqualHtml(`
-      <pos-container-contents>
+      expect(page.root.shadowRoot).toEqualHtml(`
         <pos-container-toolbar></pos-container-toolbar>
         <ul aria-label="Container contents">
           <li><pos-create-new-container-item type="file"></pos-create-new-container-item></li>
         </ul>
-      </pos-container-contents>
   `);
 
       // and the current container is passed into it
-      const createNew: PosCreateNewContainerItem = page.root.querySelector('pos-create-new-container-item');
+      const createNew: PosCreateNewContainerItem = page.root.shadowRoot.querySelector('pos-create-new-container-item');
       expect(createNew.container).toEqual(container);
     });
 
     it('shows input to create a new container when event occurs', async () => {
       // when the toolbar fires a create-new-folder event
-      const toolbar = page.root.querySelector('pos-container-toolbar');
+      const toolbar = page.root.shadowRoot.querySelector('pos-container-toolbar');
       expect(toolbar).not.toBeNull();
       fireEvent(toolbar, new CustomEvent('pod-os:create-new-folder', {}));
       await page.waitForChanges();
 
       // then the input to create a new folder is shown
-      expect(page.root).toEqualHtml(`
-      <pos-container-contents>
+      expect(page.root.shadowRoot).toEqualHtml(`
         <pos-container-toolbar></pos-container-toolbar>
         <ul aria-label="Container contents">
           <li><pos-create-new-container-item type="folder"></pos-create-new-container-item></li>
         </ul>
-      </pos-container-contents>
   `);
 
       // and the current container is passed into it
-      const createNew: PosCreateNewContainerItem = page.root.querySelector('pos-create-new-container-item');
+      const createNew: PosCreateNewContainerItem = page.root.shadowRoot.querySelector('pos-create-new-container-item');
       expect(createNew.container).toEqual(container);
     });
 
     it('hides input to create a new file or folder when escape is pressed', async () => {
       // and a new file should be created
-      page.rootInstance.createNewItem = 'file';
+      await createNewFile(page);
       expect(page.root.querySelector('pos-create-new-container-item')).toBeDefined();
 
       // when the user presses escape
@@ -270,3 +234,10 @@ describe('pos-container-contents', () => {
     });
   });
 });
+
+async function createNewFile(page: any) {
+  const toolbar = page.root.shadowRoot!.querySelector('pos-container-toolbar')!;
+  expect(toolbar).not.toBeNull();
+  fireEvent(toolbar, new CustomEvent('pod-os:create-new-file', {}));
+  await page.waitForChanges();
+}
