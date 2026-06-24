4c74842b54c09de2eee329af1334f0afb4d39369
Angelo <angelo+github.com@veltens.org>
2026-06-24 19:27:26 +0200

    test: migrate pos-list fetch integration tests to vitest

---
+++ elements/src/components/pos-list/pos-list-fetch.integration.vspec.tsx
@@ -0,0 +1,40 @@
+import { describe, expect, h, it, render } from '@stencil/vitest';
+import { server, turtleFile } from '../../test/msw';
+
+describe('pos-list', () => {
+  // given a resource with a video relation pointing to two video resources
+  it('fetches resources if fetch attribute is present', async () => {
+    server.use(
+      turtleFile(
+        'https://resource.test',
+        `
+          <> <http://schema.org/video>
+              <https://video.test/video-1>,
+              <https://video.test/video-2> .
+        `,
+      ),
+      turtleFile('https://video.test/video-1', `<> <http://schema.org/name> "Video 1" .`),
+      turtleFile('https://video.test/video-2', `<> <http://schema.org/name> "Video 2" .`),
+    );
+
+    // when a page lists all video relations of that resource, rendering a pos-label for each
+    const page = await render(
+      <pos-app>
+        <pos-resource uri="https://resource.test">
+          <pos-list rel="http://schema.org/video" fetch>
+            <template>
+              <pos-label />
+            </template>
+          </pos-list>
+        </pos-resource>
+      </pos-app>,
+    );
+    await page.waitForChanges();
+
+    // then the labels of both videos show up
+    const video1 = page.root.querySelector('pos-resource[about="https://video.test/video-1"]');
+    const video2 = page.root.querySelector('pos-resource[about="https://video.test/video-2"]');
+    expect(video1).toHaveTextContent('Video 1');
+    expect(video2).toHaveTextContent('Video 2');
+  });
+});
--- elements/src/test/msw.ts
+++ elements/src/test/msw.ts
@@ -1,3 +1,12 @@
 import { setupServer } from 'msw/node';
+import { http, HttpResponse } from 'msw';
 
 export const server = setupServer();
+
+export function turtleFile(url: string, content: string) {
+  return http.get(url, async () => {
+    const response = HttpResponse.text(content);
+    response.headers.set('Content-Type', 'text/turtle');
+    return response;
+  });
+}
