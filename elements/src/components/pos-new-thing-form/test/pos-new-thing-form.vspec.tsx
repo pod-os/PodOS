bf56b0e4276e59d1fb2dcff81e1268e7f09d217c
Angelo <angelo+github.com@veltens.org>
2026-06-30 20:55:21 +0200

    test: migrate pos-new-thing-form tests to vitest

--- elements/src/components/pos-new-thing-form/test/pos-new-thing-form.vspec.tsx
+++ elements/src/components/pos-new-thing-form/test/pos-new-thing-form.vspec.tsx
@@ -1,59 +1,48 @@
+import { Mock, vi } from 'vitest';
+import { beforeEach, describe, expect, h, it, render, RenderResult } from '@stencil/vitest';
 import { PodOS } from '@pod-os/core';
-import { newSpecPage } from '@stencil/core/testing';
-import { fireEvent, screen } from '@testing-library/dom';
-import { when } from 'jest-when';
-import { mockPodOS } from '../../../test/mockPodOS';
-import { PosNewThingForm } from '../pos-new-thing-form';
+import { fireEvent } from '@testing-library/dom';
+import { when } from 'vitest-when';
+import { mockPodOS } from '../../../test/mockPodOS.vitest';
+import '../pos-new-thing-form';
+import { withinShadow } from '../../../test/withinShadow';
+import { userEvent } from '@testing-library/user-event';
 
 describe('pos-new-thing-form', () => {
   it('renders a form', async () => {
-    const page = await newSpecPage({
-      components: [PosNewThingForm],
-      html: `<pos-new-thing-form reference-uri="https://pod.test/container/"></pos-new-thing-form>`,
-      supportsShadowDom: false,
-    });
+    const page = await render(<pos-new-thing-form referenceUri="https://pod.test/container/"></pos-new-thing-form>);
 
-    expect(page.root).toEqualHtml(
+    expect(page.root.shadowRoot).toEqualHtml(
       `
-        <pos-new-thing-form reference-uri="https://pod.test/container/">
           <form method="dialog">
             <label for="type">
               Type
             </label>
-            <pos-select-term id="type" placeholder=""></pos-select-term>
+            <pos-select-term id="type" placeholder></pos-select-term>
             <label for="name">
               Name
             </label>
             <input id="name" type="text">
             <input id="create" type="submit" value="Create" disabled>
-          </form>
-        </pos-new-thing-form>`,
+          </form>`,
     );
   });
 
   describe('enabling and disabling the create button', () => {
     it('is disabled initially', async () => {
-      await newSpecPage({
-        components: [PosNewThingForm],
-        html: `<pos-new-thing-form reference-uri="https://pod.test/container/"></pos-new-thing-form>`,
-        supportsShadowDom: false,
-      });
-      const button: HTMLButtonElement = screen.getByDisplayValue('Create');
+      const page = await render(<pos-new-thing-form referenceUri="https://pod.test/container/"></pos-new-thing-form>);
+      const button: HTMLButtonElement = withinShadow(page).getByDisplayValue('Create');
 
       expect(button.disabled).toBe(true);
     });
 
     it('is still disabled after selecting a term', async () => {
       // given
-      const page = await newSpecPage({
-        components: [PosNewThingForm],
-        html: `<pos-new-thing-form reference-uri="https://pod.test/container/"></pos-new-thing-form>`,
-        supportsShadowDom: false,
-      });
-      const button: HTMLButtonElement = screen.getByDisplayValue('Create');
+      const page = await render(<pos-new-thing-form referenceUri="https://pod.test/container/"></pos-new-thing-form>);
+      const button: HTMLButtonElement = withinShadow(page).getByDisplayValue('Create');
 
       // when user selects a term
-      const termSelect = page.root.querySelector('pos-select-term');
+      const termSelect = page.root.shadowRoot!.querySelector('pos-select-term')!;
       fireEvent(
         termSelect,
         new CustomEvent('pod-os:term-selected', { detail: { uri: 'https://schema.org/description' } }),
@@ -65,68 +54,56 @@ describe('pos-new-thing-form', () => {
 
     it('is still disabled after entering a name', async () => {
       // given
-      const page = await newSpecPage({
-        components: [PosNewThingForm],
-        html: `<pos-new-thing-form reference-uri="https://pod.test/container/"></pos-new-thing-form>`,
-        supportsShadowDom: false,
-      });
+      const page = await render(<pos-new-thing-form referenceUri="https://pod.test/container/"></pos-new-thing-form>);
 
       const os = mockPodOS();
-      await page.rootInstance.receivePodOs(os);
+      await page.instance.receivePodOs(os);
 
       // when user fills in a name
-      const nameField = page.root.querySelector('input[type=text]');
+      const nameField = page.root.shadowRoot!.querySelector('input[type=text]')!;
       fireEvent.input(nameField, { target: { value: 'New Thing' } });
       await page.waitForChanges();
 
       // then the button is disabled
-      const button: HTMLButtonElement = screen.getByDisplayValue('Create');
+      const button: HTMLButtonElement = withinShadow(page).getByDisplayValue('Create');
       expect(button.disabled).toBe(true);
     });
 
     it('is enabled after a term has been selected and a name typed', async () => {
       // given
-      const page = await newSpecPage({
-        components: [PosNewThingForm],
-        html: `<pos-new-thing-form reference-uri="https://pod.test/container/"></pos-new-thing-form>`,
-        supportsShadowDom: false,
-      });
+      const page = await render(<pos-new-thing-form referenceUri="https://pod.test/container/"></pos-new-thing-form>);
       const os = mockPodOS();
-      await page.rootInstance.receivePodOs(os);
+      await page.instance.receivePodOs(os);
 
       // when user selects a term
-      const termSelect = page.root.querySelector('pos-select-term');
+      const termSelect = page.root.shadowRoot!.querySelector('pos-select-term')!;
       fireEvent(
         termSelect,
         new CustomEvent('pod-os:term-selected', { detail: { uri: 'https://schema.org/description' } }),
       );
 
       // and fills in a name
-      const nameField = page.root.querySelector('input[type=text]');
+      const nameField = page.root.shadowRoot!.querySelector('input[type=text]')!;
       fireEvent.input(nameField, { target: { value: 'New Thing' } });
       await page.waitForChanges();
 
       // then the button is enabled
-      const button: HTMLButtonElement = screen.getByDisplayValue('Create');
+      const button: HTMLButtonElement = withinShadow(page).getByDisplayValue('Create');
       expect(button.disabled).toBe(false);
     });
 
     it('is enabled when name is typed and then a term is selected', async () => {
       // given
-      const page = await newSpecPage({
-        components: [PosNewThingForm],
-        html: `<pos-new-thing-form reference-uri="https://pod.test/container/"></pos-new-thing-form>`,
-        supportsShadowDom: false,
-      });
+      const page = await render(<pos-new-thing-form referenceUri="https://pod.test/container/"></pos-new-thing-form>);
       const os = mockPodOS();
-      await page.rootInstance.receivePodOs(os);
+      await page.instance.receivePodOs(os);
 
       // when user fills in a name
-      const nameField = page.root.querySelector('input[type=text]');
+      const nameField = page.root.shadowRoot!.querySelector('input[type=text]')!;
       fireEvent.input(nameField, { target: { value: 'New Thing' } });
 
       // and after that selects a term
-      const termSelect = page.root.querySelector('pos-select-term');
+      const termSelect = page.root.shadowRoot!.querySelector('pos-select-term')!;
       fireEvent(
         termSelect,
         new CustomEvent('pod-os:term-selected', { detail: { uri: 'https://schema.org/description' } }),
@@ -134,87 +111,78 @@ describe('pos-new-thing-form', () => {
       await page.waitForChanges();
 
       // then the button is enabled
-      const button: HTMLButtonElement = screen.getByDisplayValue('Create');
+      const button: HTMLButtonElement = withinShadow(page).getByDisplayValue('Create');
       expect(button.disabled).toBe(false);
     });
   });
 
   it('shows the URI of the new thing after typing a name', async () => {
-    const page = await newSpecPage({
-      components: [PosNewThingForm],
-      html: `<pos-new-thing-form reference-uri="https://pod.test/container/"></pos-new-thing-form>`,
-      supportsShadowDom: false,
-    });
+    const page = await render(<pos-new-thing-form referenceUri="https://pod.test/container/"></pos-new-thing-form>);
 
     const os = mockPodOS();
     when(os.proposeUriForNewThing)
       .calledWith('https://pod.test/container/', 'New Thing')
-      .mockReturnValue('https://pod.test/container/new-thing#it');
-    await page.rootInstance.receivePodOs(os);
+      .thenReturn('https://pod.test/container/new-thing#it');
+    await page.instance.receivePodOs(os);
 
-    const nameField = page.root.querySelector('input[type=text]');
-    fireEvent.input(nameField, { target: { value: 'New Thing' } });
+    const nameField = page.root.shadowRoot!.querySelector('input[type=text]')!;
+    await userEvent.type(nameField, 'New Thing');
+    // fireEvent.input(nameField, { target: { value: 'New Thing' } });
 
     await page.waitForChanges();
 
     expect(os.proposeUriForNewThing).toHaveBeenCalledWith('https://pod.test/container/', 'New Thing');
 
-    expect(page.root).toEqualHtml(
+    expect(page.root.shadowRoot).toEqualHtml(
       `
-        <pos-new-thing-form reference-uri="https://pod.test/container/">
           <form method="dialog">
             <label for="type">
               Type
             </label>
-            <pos-select-term id="type" placeholder=""></pos-select-term>
+            <pos-select-term id="type" placeholder></pos-select-term>
             <label for="name">
               Name
             </label>
-            <input id="name" type="text" value="New Thing">
+            <input id="name" type="text">
             <div id="new-uri" title="This will be the URI of the new thing">
               https://pod.test/container/new-thing#it
             </div>
-            <input id="create" type="submit" value="Create" disabled />
-          </form>
-        </pos-new-thing-form>`,
+            <input id="create" type="submit" value="Create" disabled>
+          </form>`,
     );
   });
 
   describe('submitting form', () => {
     describe('successfully', () => {
-      let page;
-      let os;
-      let linkEventListener;
+      let page: RenderResult;
+      let os: PodOS;
+      let linkEventListener: Mock;
 
       beforeEach(async () => {
         // given
-        page = await newSpecPage({
-          components: [PosNewThingForm],
-          html: `<pos-new-thing-form reference-uri="https://pod.test/container/"></pos-new-thing-form>`,
-          supportsShadowDom: false,
-        });
+        page = await render(<pos-new-thing-form referenceUri="https://pod.test/container/"></pos-new-thing-form>);
 
         os = mockPodOS();
         when(os.proposeUriForNewThing)
           .calledWith('https://pod.test/container/', 'New Thing')
-          .mockReturnValue('https://pod.test/container/new-thing#it');
-        await page.rootInstance.receivePodOs(os);
+          .thenReturn('https://pod.test/container/new-thing#it');
+        await page.instance.receivePodOs(os);
 
         // and the page listens for pod-os:link events
-        linkEventListener = jest.fn();
+        linkEventListener = vi.fn();
         page.root.addEventListener('pod-os:link', linkEventListener);
 
         // when user selects a term and adds a name
-        const termSelect = page.root.querySelector('pos-select-term');
+        const termSelect = page.root.shadowRoot!.querySelector('pos-select-term')!;
         fireEvent(termSelect, new CustomEvent('pod-os:term-selected', { detail: { uri: 'https://schema.org/Thing' } }));
 
-        const nameField = page.root.querySelector('input[type=text]');
+        const nameField = page.root.shadowRoot!.querySelector('input[type=text]')!;
         fireEvent.input(nameField, { target: { value: 'New Thing' } });
 
         await page.waitForChanges();
 
         // and submits the form
-        const form: HTMLFormElement = page.root.querySelector('form');
+        const form: HTMLFormElement = page.root.shadowRoot!.querySelector('form')!;
         fireEvent.submit(form);
         await page.waitForChanges();
       });
@@ -236,52 +204,48 @@ describe('pos-new-thing-form', () => {
       });
 
       it('resets the form', async () => {
-        expect(page.rootInstance.name).toBe('');
-        expect(page.rootInstance.newUri).toBe('');
-        expect(page.rootInstance.selectedTypeUri).toBe('');
+        expect(page.instance.name).toBe('');
+        expect(page.instance.newUri).toBe('');
+        expect(page.instance.selectedTypeUri).toBe('');
       });
     });
 
     describe('with error', () => {
-      let page;
+      let page: RenderResult;
       let os: Partial<PodOS>;
-      let linkEventListener;
-      let errorEventListener;
+      let linkEventListener: Mock;
+      let errorEventListener: Mock;
 
       beforeEach(async () => {
         // given
-        page = await newSpecPage({
-          components: [PosNewThingForm],
-          html: `<pos-new-thing-form reference-uri="https://pod.test/container/"></pos-new-thing-form>`,
-          supportsShadowDom: false,
-        });
+        page = await render(<pos-new-thing-form referenceUri="https://pod.test/container/"></pos-new-thing-form>);
 
         os = mockPodOS() as unknown as PodOS;
-        when(os.proposeUriForNewThing)
+        when(os.proposeUriForNewThing as Mock)
           .calledWith('https://pod.test/container/', 'New Thing')
-          .mockReturnValue('https://pod.test/container/new-thing#it');
-        when(os.addNewThing).mockRejectedValue(new Error('simulated error for test'));
-        await page.rootInstance.receivePodOs(os);
+          .thenReturn('https://pod.test/container/new-thing#it');
+        (os.addNewThing as Mock).mockRejectedValue(new Error('simulated error for test'));
+        await page.instance.receivePodOs(os);
 
         // and the page listens for pod-os:link events
-        linkEventListener = jest.fn();
+        linkEventListener = vi.fn();
         page.root.addEventListener('pod-os:link', linkEventListener);
 
         // and the page listens for pod-os:error events
-        errorEventListener = jest.fn();
+        errorEventListener = vi.fn();
         page.root.addEventListener('pod-os:error', errorEventListener);
 
         // when user selects a term and adds a name
-        const termSelect = page.root.querySelector('pos-select-term');
+        const termSelect = page.root.shadowRoot!.querySelector('pos-select-term')!;
         fireEvent(termSelect, new CustomEvent('pod-os:term-selected', { detail: { uri: 'https://schema.org/Thing' } }));
 
-        const nameField = page.root.querySelector('input[type=text]');
+        const nameField = page.root.shadowRoot!.querySelector('input[type=text]')!;
         fireEvent.input(nameField, { target: { value: 'New Thing' } });
 
         await page.waitForChanges();
 
         // and submits the form
-        const form: HTMLFormElement = page.root.querySelector('form');
+        const form: HTMLFormElement = page.root.shadowRoot!.querySelector('form')!;
         fireEvent.submit(form);
         await page.waitForChanges();
       });
@@ -299,9 +263,9 @@ describe('pos-new-thing-form', () => {
       });
 
       it('does not reset the form', () => {
-        expect(page.rootInstance.name).toBe('New Thing');
-        expect(page.rootInstance.newUri).toBe('https://pod.test/container/new-thing#it');
-        expect(page.rootInstance.selectedTypeUri).toBe('https://schema.org/Thing');
+        expect(page.instance.name).toBe('New Thing');
+        expect(page.instance.newUri).toBe('https://pod.test/container/new-thing#it');
+        expect(page.instance.selectedTypeUri).toBe('https://schema.org/Thing');
       });
     });
   });
