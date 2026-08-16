05f216c907c3bff40217238b0f4b615695d50a7c
Angelo <angelo+github.com@veltens.org>
2026-08-16 11:32:24 +0200

    chore(homepage): upgrade to latest nix archive for the build process

--- homepage/shell.nix
+++ homepage/shell.nix
@@ -1,7 +1,7 @@
 let
   nixpkgs = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/fabb8c9deee281e50b1065002c9828f2cf7b2239.tar.gz";
-    sha256 = "15gvdgdqsxjjihq1r66qz1q97mlcaq1jbpkhbx287r5py2vy38b1";
+    url = "https://github.com/NixOS/nixpkgs/archive/02e08985a27c65ffd33d434eeb2e660a2e4dc84d.tar.gz";
+    sha256 = "1959piz48qhaaqdyr2m5mf92gnxxhrzhls6z1ppy01ckh5wdrya2";
   };
 in
 { pkgs ? import nixpkgs {} }:
