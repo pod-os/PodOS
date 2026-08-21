a84628ade70052c3480fc10bd07a366cb89663f6
Angelo <angelo+github.com@veltens.org>
2026-08-21 23:21:26 +0200

    feat(pos-literal): update value only after a debounced time

--- elements/src/components/pos-literals/processEdits.spec.ts
+++ elements/src/components/pos-literals/processEdits.spec.ts
@@ -1,10 +1,17 @@
-import { describe, expect, it } from 'vitest';
+import { afterEach, describe, expect, it, vi } from 'vitest';
 import { LiteralChanged, processEdits } from './processEdits';
 import { mockPodOS } from '../../test/mockPodOS.vitest';
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
 import { Thing } from '@pod-os/core';
+import { beforeEach } from '@stencil/vitest';
 
 describe('process edits', () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+  afterEach(() => {
+    vi.useRealTimers();
+  });
   it('processes a single edit', () => {
     // given os and resource
     const os = mockPodOS();
@@ -24,6 +31,48 @@ describe('process edits', () => {
       'http://schema.org/name',
       'Old Name',
       'New Name',
+    );
+  });
+
+  it('processes multiple edits with a debounce', () => {
+    // given os and resource
+    const os = mockPodOS();
+    const resource = {} as Thing;
+
+    // when multiple edits are done quickly before the debounce time is reached
+    const edits$: Subject<LiteralChanged> = new Subject();
+    edits$.pipe(processEdits(os, resource)).subscribe();
+    edits$.next({
+      predicate: 'http://schema.org/name',
+      oldValue: 'Old Name',
+      newValue: 'Name 1',
+    });
+    vi.advanceTimersByTime(999);
+    edits$.next({
+      predicate: 'http://schema.org/name',
+      oldValue: 'Old Name',
+      newValue: 'Name 2',
+    });
+    vi.advanceTimersByTime(999);
+    edits$.next({
+      predicate: 'http://schema.org/name',
+      oldValue: 'Old Name',
+      newValue: 'Name 3',
+    });
+    vi.advanceTimersByTime(999);
+
+    // then the os does not update anything yet
+    expect(os.editPropertyValue).not.toHaveBeenCalled();
+
+    // but when the debounce-threshold is reached
+    vi.advanceTimersByTime(1);
+
+    // then the os updates the property value once with the latest change
+    expect(os.editPropertyValue).toHaveBeenCalledExactlyOnceWith(
+      resource,
+      'http://schema.org/name',
+      'Old Name',
+      'Name 3',
     );
   });
 });
