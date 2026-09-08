dbec1b33709bf8556fd155dd8f856bdce1ff06f2
Angelo <angelo+github.com@veltens.org>
2026-09-08 20:57:49 +0200

    test(pos-literals): end to end test for editing literal values

--- apps/tests/page-objects/Literals.ts
+++ apps/tests/page-objects/Literals.ts
@@ -21,4 +21,29 @@ export class Literals {
       .getByRole("definition")
       .describe(`Value of "${label}"`);
   }
+
+  /**
+   * Text indicating the editing status
+   */
+  fieldStatus(label: string): Locator {
+    return this.valueOf(label)
+      .getByRole("status")
+      .describe(`Editing status of "${label}"`);
+  }
+
+  /**
+   * The textbox to edit the literal value
+   */
+  editorOf(label: string): Locator {
+    return this.valueOf(label)
+      .getByRole("textbox")
+      .describe(`Editor for "${label}"`);
+  }
+
+  /**
+   * Change the literal value to a new value
+   */
+  async editValue(label: string, newValue: string): Promise<void> {
+    await this.editorOf(label).fill(newValue);
+  }
 }
