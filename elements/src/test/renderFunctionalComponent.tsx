95760957a201c92f31b98a2cb9fbb074fa2f824d
Angelo <angelo+github.com@veltens.org>
2026-09-08 21:37:23 +0200

    chore: remove obsolete function and test component

--- elements/src/test/renderFunctionalComponent.tsx
+++
@@ -1,9 +0,0 @@
-import { newSpecPage } from '@stencil/core/testing';
-import { TestComponent } from './TestComponent';
-
-export function renderFunctionalComponent(jsx: any) {
-  return newSpecPage({
-    components: [TestComponent],
-    template: () => jsx,
-  });
-}
