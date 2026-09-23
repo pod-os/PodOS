48cc1a0ee4ce6b7b9a1c67c5c3419596876452c6
Angelo <angelo+github.com@veltens.org>
2026-09-23 21:57:28 +0200

    chore: remove obsolete jest config

--- core/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @returns {Promise<import('jest').Config>} */
-export default async () => {
-  return {
-    transformIgnorePatterns: [
-      "/node_modules/(?!(@solid-data-modules|mime|url-template)/)",
-    ],
-  };
-};
